feae85c523f4074a89ecaa0b3c998d010a29f5bc
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-15 13:40:48 +0000

    initial commit

---
+++ xml/src/jlibs/xml/xsd/XSUtil.java
@@ -0,0 +1,33 @@
+package jlibs.xml.xsd;
+
+import jlibs.xml.sax.MyNamespaceSupport;
+import org.apache.xerces.xs.XSModel;
+import org.apache.xerces.xs.StringList;
+import org.apache.xerces.xs.XSObject;
+import org.apache.xerces.xs.XSAttributeUse;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class XSUtil{
+    public static MyNamespaceSupport createNamespaceSupport(XSModel model){
+        MyNamespaceSupport nsSupport = new MyNamespaceSupport();
+        StringList list = model.getNamespaces();
+        for(int i=0; i<list.getLength(); i++){
+            if(list.item(i)!=null) // default namespace is null
+                nsSupport.declarePrefix(list.item(i));
+        }
+        return nsSupport;
+    }
+
+    public static String getQName(XSObject obj, MyNamespaceSupport nsSupport){
+        if(obj instanceof XSAttributeUse)
+            obj = ((XSAttributeUse)obj).getAttrDeclaration();
+        
+        if(obj.getName()==null)
+            return "";
+        String ns = obj.getNamespace();
+        String prefix = nsSupport.findPrefix(ns==null ? "" : ns);
+        return prefix==null ? obj.getName() : prefix+':'+obj.getName();
+    }
+}
