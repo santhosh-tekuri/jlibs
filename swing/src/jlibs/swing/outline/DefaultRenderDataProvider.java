41478c8e7d7db4217889f20ece88bd1e009110b5
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-15 17:20:09 +0000

    font style support

--- swing/src/jlibs/swing/outline/DefaultRenderDataProvider.java
+++ swing/src/jlibs/swing/outline/DefaultRenderDataProvider.java
@@ -15,14 +15,31 @@ public class DefaultRenderDataProvider implements RenderDataProvider{
     /*-------------------------------------------------[ DisplayName ]---------------------------------------------------*/
     
     private Visitor<Object, String> displayNameVisitor;
+    private Visitor<Object, Integer> fontStyleVisitor;
 
     public void setDisplayNameVisitor(Visitor<Object, String> displayNameVisitor){
         this.displayNameVisitor = displayNameVisitor;
     }
 
+    public void setFontStyleVisitor(Visitor<Object, Integer> fontStyleVisitor){
+        this.fontStyleVisitor = fontStyleVisitor;
+    }
+
     @Override
     public String getDisplayName(Object obj){
-        return displayNameVisitor!=null ? displayNameVisitor.visit(obj) : null;
+        String str = displayNameVisitor != null ? displayNameVisitor.visit(obj) : null;
+        if(fontStyleVisitor!=null){
+            int style = fontStyleVisitor.visit(obj);
+            if((style!=Font.PLAIN)){
+                str = str.replace("<", "&lt;");
+                if((style&Font.BOLD)!=0)
+                    str = "<b>"+str+"</b>";
+                if((style&Font.ITALIC)!=0)
+                    str = "<i>"+str+"</i>";
+                str = "<html><body>"+str+"</body></html>";
+            }
+        }
+        return str;
     }
 
     /*-------------------------------------------------[ Background ]---------------------------------------------------*/
