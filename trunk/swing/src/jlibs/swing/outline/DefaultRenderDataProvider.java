58a56cd6a64305d34f9f0e6b0f8cb1abfa9977e2
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-15 13:34:14 +0000

    visitors for foreground and background

--- trunk/swing/src/jlibs/swing/outline/DefaultRenderDataProvider.java
+++ trunk/swing/src/jlibs/swing/outline/DefaultRenderDataProvider.java
@@ -12,6 +12,8 @@ import jlibs.swing.EmptyIcon;
  * @author Santhosh Kumar T
  */
 public class DefaultRenderDataProvider implements RenderDataProvider{
+    /*-------------------------------------------------[ DisplayName ]---------------------------------------------------*/
+    
     private Visitor<Object, String> displayNameVisitor;
 
     public void setDisplayNameVisitor(Visitor<Object, String> displayNameVisitor){
@@ -23,19 +25,37 @@ public class DefaultRenderDataProvider implements RenderDataProvider{
         return displayNameVisitor!=null ? displayNameVisitor.visit(obj) : null;
     }
 
+    /*-------------------------------------------------[ Background ]---------------------------------------------------*/
+    
+    private Visitor<Object, Color> backgroundVisitor;
+
+    public void setBackgroundVisitor(Visitor<Object, Color> backgroundVisitor){
+        this.backgroundVisitor = backgroundVisitor;
+    }
+
+    @Override
+    public Color getBackground(Object obj){
+        return backgroundVisitor!=null ? backgroundVisitor.visit(obj) : null;
+    }
+
+    /*-------------------------------------------------[ Foreground ]---------------------------------------------------*/
+
+    private Visitor<Object, Color> foregroundVisitor;
+
+    public void setForegroundVisitor(Visitor<Object, Color> foregroundVisitor){
+        this.foregroundVisitor = foregroundVisitor;
+    }
+
+    @Override
+    public Color getForeground(Object obj){
+        return foregroundVisitor!=null ? foregroundVisitor.visit(obj) : null;
+    }
+
+    /*-------------------------------------------------[ Others ]---------------------------------------------------*/
+    
     @Override
     public boolean isHtmlDisplayName(Object o){
         return false;
-    }
-
-    @Override
-    public Color getBackground(Object o){
-        return null;
-    }
-
-    @Override
-    public Color getForeground(Object o){
-        return null;
     }
 
     @Override
