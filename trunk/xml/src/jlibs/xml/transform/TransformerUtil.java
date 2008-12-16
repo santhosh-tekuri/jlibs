066847e9d157a0f5418d6ec82db81f46cba7e795
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-16 14:47:26 +0000

    initial commit

---
+++ trunk/xml/src/jlibs/xml/transform/TransformerUtil.java
@@ -0,0 +1,30 @@
+package jlibs.xml.transform;
+
+import jlibs.core.lang.StringUtil;
+
+import javax.xml.transform.*;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class TransformerUtil{
+    public static final String OUTPUT_KEY_INDENT_AMOUT = "{http://xml.apache.org/xslt}indent-amount";
+
+    public static Transformer newTransformer(Source source, boolean omitXMLDeclaration, int indentAmount, String encoding) throws TransformerConfigurationException{
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = source!=null ? factory.newTransformer(source) : factory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, omitXMLDeclaration ? "yes" : "no");
+
+        // indentation
+        if(indentAmount>=0){
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            if(indentAmount>0)
+                transformer.setOutputProperty(OUTPUT_KEY_INDENT_AMOUT, String.valueOf(indentAmount));
+        }
+
+        if(!StringUtil.isWhitespace(encoding))
+            transformer.setOutputProperty(OutputKeys.ENCODING, encoding.trim());
+        
+        return transformer;
+    }
+}
