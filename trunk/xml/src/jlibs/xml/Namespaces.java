efd799b65d0b5607c18131c299b31b987541d6a7
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-16 02:05:52 +0000

    * changed from interface to class * used properties instead of NamespaceSupport

--- trunk/xml/src/jlibs/xml/Namespaces.java
+++ trunk/xml/src/jlibs/xml/Namespaces.java
@@ -1,9 +1,9 @@
 package jlibs.xml;
 
 import jlibs.core.lang.ImpossibleException;
-import jlibs.xml.sax.MyNamespaceSupport;
 
 import java.lang.reflect.Field;
+import java.util.Properties;
 
 /**
  * Interface containing commonly used namespaces and their common prefixes.
@@ -12,54 +12,66 @@ import java.lang.reflect.Field;
  *             String URI_<suggestedPrefixInUppercase> = "<namespaceURI>
  * @author Santhosh Kumar T
  */
-public interface Namespaces{
-    String URI_XML   = "http://www.w3.org/XML/1998/namespace";                     //NOI18N
-    String URI_XMLNS   = "http://www.w3.org/2000/xmlns/";                          //NOI18N
+public class Namespaces{
+    public static final String URI_XML   = "http://www.w3.org/XML/1998/namespace";                     //NOI18N
+    public static final String URI_XMLNS   = "http://www.w3.org/2000/xmlns/";                          //NOI18N
 
     /** Schema namespace as defined by XSD **/
-    String URI_XSD   = "http://www.w3.org/2001/XMLSchema";                         //NOI18N
+    public static final String URI_XSD   = "http://www.w3.org/2001/XMLSchema";                         //NOI18N
 
     /** Instance namespace as defined by XSD **/
-    String URI_XSI   = "http://www.w3.org/2001/XMLSchema-instance";                //NOI18N
+    public static final String URI_XSI   = "http://www.w3.org/2001/XMLSchema-instance";                //NOI18N
 
 
-    String URI_XSL   = "http://www.w3.org/1999/XSL/Transform";                     //NOI18N
-    String URI_BPWS  = "http://schemas.xmlsoap.org/ws/2003/03/business-process/";  //NOI18N
-    String URI_FESB  = "http://www.fiorano.com/ws/2005/08/business-process/";      //NOI18N
-    String URI_XHTML = "http://www.w3.org/1999/xhtml";                             //NOI18N
-    String URI_XPATH  = "http://www.w3.org/TR/1999/REC-xpath-19991116";            //NOI18N
-    String URI_PLINK = "http://schemas.xmlsoap.org/ws/2003/05/partner-link/";      //NOI18N
+    public static final String URI_XSL   = "http://www.w3.org/1999/XSL/Transform";                     //NOI18N
+    public static final String URI_BPWS  = "http://schemas.xmlsoap.org/ws/2003/03/business-process/";  //NOI18N
+    public static final String URI_FESB  = "http://www.fiorano.com/ws/2005/08/business-process/";      //NOI18N
+    public static final String URI_XHTML = "http://www.w3.org/1999/xhtml";                             //NOI18N
+    public static final String URI_XPATH  = "http://www.w3.org/TR/1999/REC-xpath-19991116";            //NOI18N
+    public static final String URI_PLINK = "http://schemas.xmlsoap.org/ws/2003/05/partner-link/";      //NOI18N
 
     /** WSDL namespace for WSDL framework **/
-    String URI_WSDL  = "http://schemas.xmlsoap.org/wsdl/";                         //NOI18N
+    public static final String URI_WSDL  = "http://schemas.xmlsoap.org/wsdl/";                         //NOI18N
 
     /** WSDL namespace for WSDL HTTP GET & POST binding **/
-    String URI_HTTP = "http://schemas.xmlsoap.org/wsdl/http/"; //NOI18N
+    public static final String URI_HTTP = "http://schemas.xmlsoap.org/wsdl/http/"; //NOI18N
 
     /** WSDL namespace for WSDL MIME binding **/
-    String URI_MIME = "http://schemas.xmlsoap.org/wsdl/mime/"; //NOI18N
+    public static final String URI_MIME = "http://schemas.xmlsoap.org/wsdl/mime/"; //NOI18N
 
     /**	WSDL namespace for WSDL SOAP binding **/
-    String URI_SOAP = "http://schemas.xmlsoap.org/wsdl/soap/"; //NOI18N
+    public static final String URI_SOAP = "http://schemas.xmlsoap.org/wsdl/soap/"; //NOI18N
 
     /** Encoding namespace as defined by SOAP 1.1 **/
-    String URI_SOAPENC = "http://schemas.xmlsoap.org/soap/encoding/"; //NOI18N
+    public static final String URI_SOAPENC = "http://schemas.xmlsoap.org/soap/encoding/"; //NOI18N
 
     /** Envelope namespace as defined by SOAP 1.1 **/
-    String URI_SOAPENV = "http://schemas.xmlsoap.org/soap/envelope/"; //NOI18N
+    public static final String URI_SOAPENV = "http://schemas.xmlsoap.org/soap/envelope/"; //NOI18N
 
-    MyNamespaceSupport SUGGESTED = new MyNamespaceSupport(){
-        {
-            for(Field field: Namespaces.class.getFields()){
-                if(field.getName().startsWith("URI_")) //NOI18N
-                    try{
-                        String prefix = field.getName().substring("URI_".length()).toLowerCase(); //NOI18N
-                        String uri = (String)field.get(null);
-                        declarePrefix(prefix, uri);
-                    } catch(IllegalAccessException ex){
-                        throw new ImpossibleException(ex);
-                    }
-            }
+    /*-------------------------------------------------[ Suggestions ]---------------------------------------------------*/
+
+    private static final Properties suggested = new Properties();
+
+    static{
+        for(Field field: Namespaces.class.getFields()){
+            if(field.getName().startsWith("URI_")) //NOI18N
+                try{
+                    String prefix = field.getName().substring("URI_".length()).toLowerCase(); //NOI18N
+                    String uri = (String)field.get(null);
+                    suggested.put(uri, prefix);
+                } catch(IllegalAccessException ex){
+                    throw new ImpossibleException(ex);
+                }
         }
-    };
+    }
+
+    public static String suggestPrefix(String uri){
+        if(uri==null)
+            uri = "";
+        return suggested.getProperty(uri);
+    }
+
+    public static Properties getSuggested(){
+        return new Properties(suggested);
+    }
 }
