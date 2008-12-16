3a326ce88974d77f548e851917ed84ec265fa7c3
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-16 02:09:47 +0000

    initial commit

---
+++ core/src/jlibs/core/lang/StringUtil.java
@@ -0,0 +1,10 @@
+package jlibs.core.lang;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class StringUtil{
+    public static boolean isEmpty(String str){
+        return str==null || str.isEmpty();
+    }
+}
