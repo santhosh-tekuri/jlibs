a08fe314d5c36f16a369b55ea58764e7409bb061
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-21 12:20:44 +0000

    initial commit

---
+++ trunk/swing/src/jlibs/swing/xml/xsd/XSDOutlinePanel.java
@@ -0,0 +1,62 @@
+package jlibs.swing.xml.xsd;
+
+import org.netbeans.swing.outline.Outline;
+import org.netbeans.swing.outline.RowModel;
+import org.netbeans.swing.outline.DefaultOutlineModel;
+import org.apache.xerces.xs.XSModel;
+
+import javax.swing.*;
+import javax.swing.tree.TreeModel;
+import java.awt.*;
+
+import jlibs.core.graph.Navigator;
+import jlibs.core.graph.Path;
+import jlibs.core.graph.navigators.FilteredTreeNavigator;
+import jlibs.core.graph.navigators.PathNavigator;
+import jlibs.swing.tree.NavigatorTreeModel;
+import jlibs.swing.outline.DefaultRowModel;
+import jlibs.swing.outline.DefaultColumn;
+import jlibs.swing.outline.DefaultRenderDataProvider;
+import jlibs.xml.sax.helpers.MyNamespaceSupport;
+import jlibs.xml.xsd.XSUtil;
+import jlibs.xml.xsd.XSNavigator;
+import jlibs.xml.xsd.display.*;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class XSDOutlinePanel extends JPanel{
+    protected Outline outline = new Outline();
+    
+    public XSDOutlinePanel(){
+        super(new BorderLayout());
+
+        add(new JScrollPane(outline), BorderLayout.CENTER);
+        outline.setShowGrid(false);
+        outline.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
+    }
+
+    public void setXSModel(XSModel model){
+        setXSModel(model, XSUtil.createNamespaceSupport(model));
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public void setXSModel(XSModel model, MyNamespaceSupport nsSupport){
+        Navigator navigator1 = new FilteredTreeNavigator(new XSNavigator(), new XSDisplayFilter());
+        Navigator navigator = new PathNavigator(navigator1);
+        XSPathDiplayFilter filter = new XSPathDiplayFilter(navigator1);
+        navigator = new FilteredTreeNavigator(navigator, filter);
+        
+        TreeModel treeModel = new NavigatorTreeModel(new Path(model), navigator);
+        RowModel rowModel = new DefaultRowModel(new DefaultColumn("Detail", String.class, new XSDisplayValueVisitor(nsSupport))/*, new ClassColumn()*/);
+
+        outline.setModel(DefaultOutlineModel.createOutlineModel(treeModel, rowModel));
+        outline.getColumnModel().getColumn(1).setMinWidth(150);
+
+        DefaultRenderDataProvider dataProvider = new DefaultRenderDataProvider();
+        dataProvider.setDisplayNameVisitor(new XSDisplayNameVisitor(nsSupport, filter));
+        dataProvider.setForegroundVisitor(new XSColorVisitor(filter));
+        dataProvider.setFontStyleVisitor(new XSFontStyleVisitor(filter));
+        outline.setRenderDataProvider(dataProvider);
+    }
+}
