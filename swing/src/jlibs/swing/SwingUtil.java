7fdbc84e7ebff2f6615b253f6fc3d4c132f037f8
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-24 12:16:07 +0000

    added doAction()

--- swing/src/jlibs/swing/SwingUtil.java
+++ swing/src/jlibs/swing/SwingUtil.java
@@ -1,8 +1,11 @@
 package jlibs.swing;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author Santhosh Kumar T
@@ -16,5 +19,19 @@ public class SwingUtil{
                 we.getWindow().removeWindowFocusListener(this);
             }
         });
+    }
+
+    @SuppressWarnings({"UnusedAssignment"})
+    public static void doAction(JTextField textField){
+        String command = null;
+        if(textField.getAction()!=null)
+            command = (String)textField.getAction().getValue(Action.ACTION_COMMAND_KEY);
+        ActionEvent event = null;
+        
+        for(ActionListener listener: textField.getActionListeners()){
+            if(event==null)
+                event = new ActionEvent(textField, ActionEvent.ACTION_PERFORMED, command, System.currentTimeMillis(), 0);
+            listener.actionPerformed(event);
+        }
     }
 }
