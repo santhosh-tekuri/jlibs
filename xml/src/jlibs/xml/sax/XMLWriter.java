c158fb9eed2bdb4512bf41005923a546038bb0f1
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-31 12:06:39 +0000

    handle ParserConfigurationException thrown by writeInto(...)

--- xml/src/jlibs/xml/sax/XMLWriter.java
+++ xml/src/jlibs/xml/sax/XMLWriter.java
@@ -3,6 +3,7 @@ package jlibs.xml.sax;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 /**
@@ -15,11 +16,15 @@ public class XMLWriter extends AbstractXMLReader{
 
     @Override
     @SuppressWarnings({"unchecked"})
-    public void parse(InputSource input) throws IOException, SAXException{
-        if(input instanceof ObjectInputSource)
-            ((ObjectInputSource)input).writeInto(this);
-        else
-            throw new IOException("can't parse "+input);
+    public void parse(InputSource input) throws SAXException, IOException{
+        try{
+            if(input instanceof ObjectInputSource)
+                ((ObjectInputSource)input).writeInto(this);
+            else
+                throw new IOException("can't parse "+input);
+        }catch(ParserConfigurationException ex){
+            throw new SAXException(ex);
+        }
     }
 
     @Override
