f897ea2c0b631790d730a1492488a4290018220e
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2008-12-31 12:07:24 +0000

    added helper methods

--- xml/src/jlibs/xml/sax/helpers/NamespaceSupportReader.java
+++ xml/src/jlibs/xml/sax/helpers/NamespaceSupportReader.java
@@ -2,13 +2,30 @@ package jlibs.xml.sax.helpers;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.InputSource;
 import org.xml.sax.helpers.XMLFilterImpl;
+import org.xml.sax.helpers.DefaultHandler;
+
+import java.io.IOException;
+
+import jlibs.xml.sax.SAXUtil;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 /**
  * @author Santhosh Kumar T
  */
 public class NamespaceSupportReader extends XMLFilterImpl{
-    private MyNamespaceSupport nsSupport;
+    private MyNamespaceSupport nsSupport = new MyNamespaceSupport();
+
+    public NamespaceSupportReader(boolean nsPrefixes) throws ParserConfigurationException, SAXException{
+        this(SAXUtil.newSAXParser(true, nsPrefixes).getXMLReader());
+    }
+    
+    public NamespaceSupportReader(XMLReader parent){
+        super(parent);
+    }
 
     public MyNamespaceSupport getNamespaceSupport(){
         return nsSupport;
@@ -17,7 +34,7 @@ public class NamespaceSupportReader extends XMLFilterImpl{
     private boolean needNewContext;
     @Override
     public void startDocument() throws SAXException{
-        nsSupport = new MyNamespaceSupport();
+        nsSupport.reset();
         needNewContext = true;
         super.startDocument();
     }
@@ -42,5 +59,27 @@ public class NamespaceSupportReader extends XMLFilterImpl{
     public void endElement(String uri, String localName, String qName) throws SAXException{
         nsSupport.popContext();
         super.endElement(uri, localName, qName);
+    }
+
+    public void setDefaultHandler(DefaultHandler handler){
+        if(handler instanceof SAXHandler)
+            ((SAXHandler)handler).nsSupport = nsSupport;
+        
+        setContentHandler(handler);
+        setEntityResolver(handler);
+        setErrorHandler(handler);
+        setDTDHandler(handler);
+    }
+
+    /*-------------------------------------------------[ Parsing ]---------------------------------------------------*/
+    
+    public void parse(InputSource is, DefaultHandler handler) throws IOException, SAXException{
+        setDefaultHandler(handler);
+        parse(is);
+    }
+
+    public void parse(String systemId, DefaultHandler handler) throws IOException, SAXException{
+        setDefaultHandler(handler);
+        parse(systemId);
     }
 }
