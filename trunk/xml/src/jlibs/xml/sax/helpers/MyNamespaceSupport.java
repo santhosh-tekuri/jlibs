4bf6dd4a859fd86280067ef8668fb04cc2bd3f86
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-01-02 14:02:08 +0000

    suggest better prefixes for given uri

--- trunk/xml/src/jlibs/xml/sax/helpers/MyNamespaceSupport.java
+++ trunk/xml/src/jlibs/xml/sax/helpers/MyNamespaceSupport.java
@@ -1,10 +1,13 @@
 package jlibs.xml.sax.helpers;
 
-import org.xml.sax.helpers.NamespaceSupport;
-import jlibs.xml.Namespaces;
 import jlibs.core.lang.Util;
+import jlibs.xml.Namespaces;
+import org.xml.sax.helpers.NamespaceSupport;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Properties;
+import java.util.StringTokenizer;
 
 /**
  * @author Santhosh Kumar T
@@ -41,15 +44,58 @@ public class MyNamespaceSupport extends NamespaceSupport{
     public String declarePrefix(String uri){
         String prefix = findPrefix(uri);
         if(prefix==null){
-            prefix = suggested.getProperty(uri);
-            if(prefix==null){
-                int i = 1;
-                while(getURI(prefix="ns"+i)!=null)
-                    i++;
-
-            }
+            prefix = suggestPrefix(uri);
             declarePrefix(prefix, uri);
         }
         return prefix;
+    }
+
+    private String suggestPrefix(String uri){
+        String prefix = suggested.getProperty(uri);
+        if(prefix!=null)
+            return prefix;
+        
+        try{
+            URI _uri = new URI(uri);
+
+            String path = _uri.getPath();
+            StringTokenizer stok = new StringTokenizer(path, "/");
+            while(stok.hasMoreTokens())
+                prefix = stok.nextToken();
+            if(prefix!=null){
+                if(getURI(prefix)==null)
+                    return prefix;
+            }else{
+                String host = _uri.getHost();
+                if(host!=null){
+                    stok = new StringTokenizer(host, ".");
+                    String curPrefix = null;
+                    while(stok.hasMoreTokens()){
+                        prefix = curPrefix;
+                        curPrefix = stok.nextToken();
+                    }
+                }                
+                if(prefix!=null){
+                    if(getURI(prefix)==null)
+                        return prefix;
+                }else
+                    prefix = "ns";
+            }
+        }catch(URISyntaxException ignore){
+            // xml spec doesn't guarantee that namespace uri should be valid uri
+        }
+        
+        int i = 1;
+        while(getURI(prefix+i)!=null)
+            i++;
+        return prefix+i;
+    }
+
+    public static void main(String[] args){
+        MyNamespaceSupport ns = new MyNamespaceSupport();
+        System.out.println(ns.declarePrefix("http://www.sonoasystems.com/schemas/2007/8/3/sci/"));
+        System.out.println(ns.declarePrefix("http://www.sonoasystems.com/schemas/2007/8/7/sci/"));
+        System.out.println(ns.declarePrefix("http://com"));
+        System.out.println(ns.declarePrefix("http://google.org"));
     }
 }
