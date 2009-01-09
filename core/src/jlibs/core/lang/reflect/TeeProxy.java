ac17e0f121f9c8a98ba2e2b5e7988dbb55d959e7
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-01-09 19:08:53 +0000

    initail commit

---
+++ core/src/jlibs/core/lang/reflect/TeeProxy.java
@@ -0,0 +1,46 @@
+/**
+ * JLibs: Common Utilities for Java
+ * Copyright (C) 2009  Santhosh Kumar T
+ * <p/>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package jlibs.core.lang.reflect;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class TeeProxy implements InvocationHandler{
+    private Object delegates[];
+
+    public TeeProxy(Object... delegates){
+        this.delegates = delegates;
+    }
+    
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
+        Object result = method.invoke(delegates[0], args);
+        for(int i=1; i<delegates.length; i++)
+            method.invoke(delegates[i], args);
+        return result;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public static <T> T create(Class<T> interfase, Object... delegates){
+        TeeProxy handler = new TeeProxy(delegates);
+        delegates[0].getClass().getInterfaces();
+        return (T)Proxy.newProxyInstance(interfase.getClassLoader(), new Class[]{ interfase }, handler);
+    }
+}
