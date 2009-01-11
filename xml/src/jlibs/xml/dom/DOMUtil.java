40e617690f7d336e6ad96b902f6404ba90c31362
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-01-11 09:07:29 +0000

    initial commig

---
+++ xml/src/jlibs/xml/dom/DOMUtil.java
@@ -0,0 +1,33 @@
+/**
+ * JLibs: Common Utilities for Java
+ * Copyright (C) 2009  Santhosh Kumar T
+ * <p/>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package jlibs.xml.dom;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class DOMUtil{
+    public static DocumentBuilder newDocumentBuilder(boolean nsAware, boolean validating, boolean coalescing) throws ParserConfigurationException{
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setNamespaceAware(nsAware);
+        factory.setValidating(validating);
+        factory.setCoalescing(coalescing);
+        return factory.newDocumentBuilder();
+    }
+}
