e62a40b734bce988fa397eec17d5f9d82ad41d5b
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-01-12 16:29:05 +0000

    reorganized context manipulation into Contexts class, so that we can add some performance importment using this

---
+++ xml/src/jlibs/xml/sax/sniff/ResettableIterator.java
@@ -0,0 +1,52 @@
+/**
+ * JLibs: Common Utilities for Java
+ * Copyright (C) 2009  Santhosh Kumar T
+ * <p/>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package jlibs.xml.sax.sniff;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class ResettableIterator<E> implements Iterator<E>{
+    private List<E> list;
+    private int cursor;
+
+    public ResettableIterator(List<E> list){
+        this.list = list;
+    }
+    
+    @Override
+    public boolean hasNext(){
+        return cursor!=list.size();
+    }
+
+    @Override
+    public E next(){
+        return list.get(cursor++);
+    }
+
+    @Override
+    public void remove(){
+        throw new UnsupportedOperationException();
+    }
+
+    public ResettableIterator<E> reset(List<E> list){
+        this.list = list;
+        cursor = 0;
+        return this;
+    }
+}
