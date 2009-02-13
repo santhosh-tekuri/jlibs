e629439fdaf3b8f046e9423d19166d65152f7eab
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-02-13 21:28:48 +0000

    rolledback changed done to consumeMemberResult(...)

--- xml/src/jlibs/xml/sax/sniff/model/expr/nodeset/Predicate.java
+++ xml/src/jlibs/xml/sax/sniff/model/expr/nodeset/Predicate.java
@@ -46,17 +46,9 @@ public class Predicate extends ValidatedExpression{
 
         @Override
         protected void consumeMemberResult(Object result){
-            if(members.get(0) instanceof Predicate || members.get(0) instanceof Node){
-                if(result instanceof Event || result==Boolean.TRUE){
-                    memberHit = true;
-                    resultPrepared();
-                }
-            }else{
-                memberHit = (Boolean)result;
-                if(memberHit)
-                    resultPrepared();
-                else
-                    setResult(false);
+            if(result instanceof Event || result==Boolean.TRUE){
+                memberHit = true;
+                resultPrepared();
             }
         }
     }
