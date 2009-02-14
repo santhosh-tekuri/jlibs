59690508d4aca32bf1deb4239ec7a227bb031dc4
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-02-14 20:34:54 +0000

    extracted Predicate Convetor into separate class

---
+++ core/src/jlibs/core/graph/PredicateConvertor.java
@@ -0,0 +1,48 @@
+/**
+ * JLibs: Common Utilities for Java
+ * Copyright (C) 2009  Santhosh Kumar T
+ * <p/>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package jlibs.core.graph;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class PredicateConvertor<E> implements Convertor<E, String>{
+    private Navigator2<E> navigator;
+    private Convertor<E, String> delegate;
+
+    public PredicateConvertor(Navigator2<E> navigator, Convertor<E, String> delegate){
+        this.navigator = navigator;
+        this.delegate = delegate;
+    }
+
+    @Override
+    public String convert(E source){
+        String name = delegate.convert(source);
+        E parent = navigator.parent(source);
+        if(parent==null)
+            return name;
+        
+        Sequence<? extends E> children = navigator.children(parent);
+        int predicate = 1;
+        while(children.hasNext()){
+            E child = children.next();
+            if(child==source)
+                break;
+            if(name.equals(delegate.convert(child)))
+                predicate++;
+        }
+        return name += "["+predicate+']';
+    }
+}
