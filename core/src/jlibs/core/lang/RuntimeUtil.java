73744cd1c131745fc67b2a784eca81d3242e2d5c
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-08-02 13:58:27 +0000

    runCommand(...) added

--- core/src/jlibs/core/lang/RuntimeUtil.java
+++ core/src/jlibs/core/lang/RuntimeUtil.java
@@ -15,6 +15,7 @@
 
 package jlibs.core.lang;
 
+import jlibs.core.io.IOPump;
 import jlibs.core.io.IOUtil;
 
 import java.io.ByteArrayOutputStream;
@@ -28,37 +29,50 @@ import java.util.StringTokenizer;
  * @author Santhosh Kumar T
  */
 public class RuntimeUtil{
-    @SuppressWarnings({"ResultOfMethodCallIgnored"})
+    public static String runCommand(String command, String[] envp, File workingDir) throws IOException{
+        String cmd[];
+        if(OS.get().isUnix())
+            cmd = new String[]{ "sh", "-c", command };
+        else
+            cmd = new String[]{ "cmd", "/C", command };
+
+        Process p = Runtime.getRuntime().exec(cmd, envp, workingDir);
+        new Thread(new IOPump(p.getErrorStream(), System.err, false, false).asRunnable()).start();
+        ByteArrayOutputStream bout = IOUtil.pump(p.getInputStream(), false);
+        try{
+            p.waitFor();
+        }catch(InterruptedException ex){
+            throw new RuntimeException("interrupted", ex);
+        }
+        if(p.exitValue()!=0)
+            throw new IOException("exitValue is "+p.exitValue());
+
+        return bout.toString();
+    }
+
+    public static String runCommand(String command) throws IOException{
+        return runCommand(command, null, null);
+    }
+
     public static String getPID() throws IOException{
         String pid = System.getProperty("pid"); //NOI18N
         if(pid==null){
-            String cmd[];
-            File tempFile = null;
-            try{
-                if(System.getProperty("os.name").toLowerCase().indexOf("windows")==-1)
-                    cmd = new String[]{ "/bin/sh", "-c", "echo $$ $PPID" }; //NOI18N
-                else{
-                    // getpids.exe is taken from http://www.scheibli.com/projects/getpids/index.html (GPL)
-                    tempFile = File.createTempFile("getpids", "exe"); //NOI18N
+            String command;
+            if(OS.get().isUnix())
+                command = "echo $$ $PPID";
+            else{
+                // getpids.exe is taken from http://www.scheibli.com/projects/getpids/index.html (GPL)
+                File tempFile = File.createTempFile("getpids", "exe"); //NOI18N
 
-                    // extract the embedded getpids.exe file from the jar and save it to above file
-                    IOUtil.pump(RuntimeUtil.class.getResourceAsStream("getpids.exe"), new FileOutputStream(tempFile), true, true); //NOI18N
-                    cmd = new String[]{ tempFile.getAbsolutePath() };
-                }
-                if(cmd!=null){
-                    Process p = Runtime.getRuntime().exec(cmd);
-                    ByteArrayOutputStream bout = new ByteArrayOutputStream();
-                    IOUtil.pump(p.getInputStream(), bout, false, true);
-
-                    StringTokenizer stok = new StringTokenizer(bout.toString());
-                    stok.nextToken(); // this is pid of the process we spanned
-                    pid = stok.nextToken();
-                    System.setProperty("pid", pid); //NOI18N
-                }
-            }finally{
-                if(tempFile!=null)
-                    tempFile.delete();
+                // extract the embedded getpids.exe file from the jar and save it to above file
+                IOUtil.pump(RuntimeUtil.class.getResourceAsStream("getpids.exe"), new FileOutputStream(tempFile), true, true); //NOI18N
+                command = tempFile.getAbsolutePath();
+                tempFile.deleteOnExit();
             }
+            StringTokenizer stok = new StringTokenizer(runCommand(command));
+            stok.nextToken(); // this is pid of the process we spanned
+            pid = stok.nextToken();
+            System.setProperty("pid", pid); //NOI18N
         }
         return pid;
     }
@@ -67,7 +81,6 @@ public class RuntimeUtil{
      * This method guarantees that garbage collection is
      * done unlike <code>{@link System#gc()}</code>
      */
-    @SuppressWarnings({"UnusedAssignment"})
     public static void gc(){
         Object obj = new Object();
         WeakReference ref = new WeakReference<Object>(obj);
