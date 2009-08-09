6cfefc5bb51c90108b78e56af5fed28d9c4f97db
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-08-09 14:42:01 +0000

    more utils

--- core/src/jlibs/core/util/CollectionUtil.java
+++ core/src/jlibs/core/util/CollectionUtil.java
@@ -15,14 +15,20 @@
 
 package jlibs.core.util;
 
+import jlibs.core.graph.Filter;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * @author Santhosh Kumar T
  */
 public class CollectionUtil{
+    /**
+     * Creates Properties from given inputStream and returns it.
+     * NOTE: the given stream is closed by this method
+     */
     public static Properties readProperties(InputStream is, Properties defaults) throws IOException{
         Properties props = new Properties(defaults);
         try{
@@ -31,5 +37,57 @@ public class CollectionUtil{
             is.close();
         }
         return props;
+    }
+
+    /**
+     * Adds objects in array to the given collection
+     *
+     * @return the same collection which is passed as argument
+     */
+    @SuppressWarnings({"unchecked", "ManualArrayToCollectionCopy"})
+    public static <E, T extends E> Collection<E> addAll(Collection<E> c, T... array){
+        for(T obj: array)
+            c.add(obj);
+        return c;
+    }
+
+    /**
+     * Removes objects in array to the given collection
+     *
+     * @return the same collection which is passed as argument
+     */
+    @SuppressWarnings("unchecked")
+    public static <E, T extends E> Collection<E> removeAll(Collection<E> c, T... array){
+        for(T obj: array)
+            c.remove(obj);
+        return c;
+    }
+
+    /**
+     * Adds the given item to the list at specified <code>index</code>.
+     * if <code>index</code> is greater than list size, it simply appends
+     * to the list.
+     */
+    public static <E, T extends E> void add(List<E> list, int index, T item){
+        if(index<list.size())
+            list.add(index, item);
+        else
+            list.add(item);
+    }
+
+    /**
+     * Returns List with elements from given collections which are selected
+     * by specified filter
+     */
+    public static <T> List<T> filter(Collection<T> c, Filter<T> filter){
+        if(c.size()==0)
+            return Collections.emptyList();
+
+        List<T> filteredList = new ArrayList<T>(c.size());
+        for(T element: c){
+            if(filter.select(element))
+                filteredList.add(element);
+        }
+        return filteredList;
     }
 }
