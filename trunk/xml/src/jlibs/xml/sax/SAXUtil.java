5fdfa9a18b4d1d0328f9b8e1815f5b14731d7516
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-08-13 02:25:13 +0000

    newSAXFactory(...) added

--- trunk/xml/src/jlibs/xml/sax/SAXUtil.java
+++ trunk/xml/src/jlibs/xml/sax/SAXUtil.java
@@ -15,27 +15,25 @@
 
 package jlibs.xml.sax;
 
-import jlibs.core.lang.ImpossibleException;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.xml.sax.SAXException;
 
 /**
  * @author Santhosh Kumar T
  */
 public class SAXUtil{
-    public static SAXParser newSAXParser(boolean namespaces, boolean nsPrefixes) throws ParserConfigurationException, SAXException{
+    public static SAXParserFactory newSAXFactory(boolean namespaces, boolean nsPrefixes) throws ParserConfigurationException, SAXException{
         SAXParserFactory factory = SAXParserFactory.newInstance();
         factory.setNamespaceAware(namespaces);
-        try{
-            if(nsPrefixes)
+        if(nsPrefixes)
             factory.setFeature(SAXFeatures.NAMESPACE_PREFIXES, true);
-        }catch(Exception ex){
-            throw new ImpossibleException(factory+" is not SAX-Compliant as it doesn't support namespace-prefixes feature");
-        }
-        return factory.newSAXParser();
+        return factory;
+    }
+
+    public static SAXParser newSAXParser(boolean namespaces, boolean nsPrefixes) throws ParserConfigurationException, SAXException{
+        return newSAXFactory(namespaces, nsPrefixes).newSAXParser();
     }
 }
