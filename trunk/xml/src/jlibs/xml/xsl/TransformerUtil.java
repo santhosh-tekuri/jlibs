143c849b0081a74eae021cdcb5140ea433548000
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-11-21 11:32:55 +0000

    - added javadoc - added newTransformerHandler() method

--- trunk/xml/src/jlibs/xml/xsl/TransformerUtil.java
+++ trunk/xml/src/jlibs/xml/xsl/TransformerUtil.java
@@ -18,16 +18,29 @@ package jlibs.xml.xsl;
 import jlibs.core.lang.StringUtil;
 
 import javax.xml.transform.*;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
 
 /**
+ * This class contains utility method related to transformation
+ *
  * @author Santhosh Kumar T
  */
 public class TransformerUtil{
     public static final String OUTPUT_KEY_INDENT_AMOUT = "{http://xml.apache.org/xslt}indent-amount";
 
-    public static Transformer newTransformer(Source source, boolean omitXMLDeclaration, int indentAmount, String encoding) throws TransformerConfigurationException{
-        TransformerFactory factory = TransformerFactory.newInstance();
-        Transformer transformer = source!=null ? factory.newTransformer(source) : factory.newTransformer();
+    /**
+     * to set various output properties on given transformer.
+     *
+     * @param transformer           transformer on which properties are set
+     * @param omitXMLDeclaration    omit xml declaration or not
+     * @param indentAmount          the number fo spaces used for indentation.
+     *                              use <=0, in case you dont want indentation
+     * @param encoding              required encoding. use null to don't set any encoding
+     *
+     * @return the same transformer which is passed as argument
+     */
+    public static Transformer setOutputProperties(Transformer transformer, boolean omitXMLDeclaration, int indentAmount, String encoding){
         transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, omitXMLDeclaration ? "yes" : "no");
 
         // indentation
@@ -39,7 +52,42 @@ public class TransformerUtil{
 
         if(!StringUtil.isWhitespace(encoding))
             transformer.setOutputProperty(OutputKeys.ENCODING, encoding.trim());
-        
+
         return transformer;
+    }
+
+    /**
+     * Creates Transformer
+     *
+     * @param source                source of xsl document, use null for identity transformer
+     * @param omitXMLDeclaration    omit xml declaration or not
+     * @param indentAmount          the number fo spaces used for indentation.
+     *                              use <=0, in case you dont want indentation
+     * @param encoding              required encoding. use null to don't set any encoding
+     *
+     * @return the same transformer which is passed as argument
+     */
+    public static Transformer newTransformer(Source source, boolean omitXMLDeclaration, int indentAmount, String encoding) throws TransformerConfigurationException{
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer = source!=null ? factory.newTransformer(source) : factory.newTransformer();
+        return setOutputProperties(transformer, omitXMLDeclaration, indentAmount, encoding);
+    }
+
+    /**
+     * Creates TransformerHandler
+     *
+     * @param source                source of xsl document, use null for identity transformer
+     * @param omitXMLDeclaration    omit xml declaration or not
+     * @param indentAmount          the number fo spaces used for indentation.
+     *                              use <=0, in case you dont want indentation
+     * @param encoding              required encoding. use null to don't set any encoding
+     *
+     * @return the same transformer which is passed as argument
+     */
+    public static TransformerHandler newTransformerHandler(Source source, boolean omitXMLDeclaration, int indentAmount, String encoding) throws TransformerConfigurationException{
+        SAXTransformerFactory factory = (SAXTransformerFactory)TransformerFactory.newInstance();
+        TransformerHandler handler = source!=null ? factory.newTransformerHandler(source) : factory.newTransformerHandler();
+        setOutputProperties(handler.getTransformer(), omitXMLDeclaration, indentAmount, encoding);
+        return handler;
     }
 }
