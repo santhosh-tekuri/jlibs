673c3259b0a969c49fd406fd4bd92a71f364ba96
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-11-28 16:10:24 +0000

    - the factory methods now take validating argument - added setHandler(reader, handler)

--- trunk/xml/src/jlibs/xml/sax/SAXUtil.java
+++ trunk/xml/src/jlibs/xml/sax/SAXUtil.java
@@ -15,7 +15,9 @@
 
 package jlibs.xml.sax;
 
-import org.xml.sax.SAXException;
+import org.xml.sax.*;
+import org.xml.sax.ext.DeclHandler;
+import org.xml.sax.ext.LexicalHandler;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -25,15 +27,45 @@ import javax.xml.parsers.SAXParserFactory;
  * @author Santhosh Kumar T
  */
 public class SAXUtil{
-    public static SAXParserFactory newSAXFactory(boolean namespaces, boolean nsPrefixes) throws ParserConfigurationException, SAXException{
+    public static SAXParserFactory newSAXFactory(boolean namespaces, boolean nsPrefixes, boolean validating) throws ParserConfigurationException, SAXException{
         SAXParserFactory factory = SAXParserFactory.newInstance();
         factory.setNamespaceAware(namespaces);
         if(nsPrefixes)
             factory.setFeature(SAXFeatures.NAMESPACE_PREFIXES, true);
+        factory.setValidating(validating);
         return factory;
     }
 
-    public static SAXParser newSAXParser(boolean namespaces, boolean nsPrefixes) throws ParserConfigurationException, SAXException{
-        return newSAXFactory(namespaces, nsPrefixes).newSAXParser();
+    public static SAXParser newSAXParser(boolean namespaces, boolean nsPrefixes, boolean validating) throws ParserConfigurationException, SAXException{
+        return newSAXFactory(namespaces, nsPrefixes, validating).newSAXParser();
+    }
+
+    /**
+     * Registers all sax hander interfaces implemented by <code>handler</code> to the
+     * specified <code>reader</reader>
+     */
+    public static void setHandler(XMLReader reader, Object handler) throws SAXNotSupportedException, SAXNotRecognizedException{
+        if(handler instanceof ContentHandler)
+            reader.setContentHandler((ContentHandler)handler);
+        if(handler instanceof EntityResolver)
+            reader.setEntityResolver((EntityResolver)handler);
+        if(handler instanceof ErrorHandler)
+            reader.setErrorHandler((ErrorHandler)handler);
+        if(handler instanceof DTDHandler)
+            reader.setDTDHandler((DTDHandler)handler);
+        if(handler instanceof LexicalHandler){
+            try{
+                reader.setProperty(SAXProperties.LEXICAL_HANDLER, handler);
+            }catch(SAXException ex){
+                reader.setProperty(SAXProperties.LEXICAL_HANDLER_ALT, handler);
+            }
+        }
+        if(handler instanceof DeclHandler){
+            try{
+                reader.setProperty(SAXProperties.DECL_HANDLER, handler);
+            }catch(SAXException ex){
+                reader.setProperty(SAXProperties.DECL_HANDLER_ALT, handler);
+            }
+        }
     }
 }
