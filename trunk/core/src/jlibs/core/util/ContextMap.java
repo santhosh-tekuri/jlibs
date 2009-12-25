c21fd4a977cd64c4814bccb2e35259daf1a26808
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-12-25 07:30:35 +0000

    contextmap

---
+++ trunk/core/src/jlibs/core/util/ContextMap.java
@@ -0,0 +1,80 @@
+/*
+ * JLibs: Common Utilities for Java
+ * Copyright (C) 2009  Santhosh Kumar T
+ * <p/>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package jlibs.core.util;
+
+import java.util.*;
+
+/**
+ * This class supports chaining of maps.
+ * If the entry is not found in current map, it will
+ * search in parent map
+ *
+ * @author Santhosh Kumar T
+ */
+public class ContextMap<K, V>{
+    public ContextMap(){
+        this(null);
+    }
+
+    public ContextMap(ContextMap<K, V> parent){
+        this.parent = parent;
+    }
+
+    private ContextMap<K, V> parent;
+    public ContextMap<K, V> parent(){
+        return parent;
+    }
+
+    private Map<K, V> map;
+    public Map<K, V> map(){
+        return map;
+    }
+
+    protected Map<K, V> createMap(){
+        return new HashMap<K,V>();
+    }
+
+    @SuppressWarnings({"SuspiciousMethodCalls"})
+    public V get(Object key){
+        ContextMap<K, V> cmap = this;
+        do{
+            if(cmap.map!=null){
+                V value = cmap.map.get(key);
+                if(value!=null)
+                    return value;
+            }
+            cmap = cmap.parent;
+        }while(cmap!=null);
+        return null;
+    }
+
+    public void put(K key, V value){
+        if(map==null)
+            map = createMap();
+        map.put(key, value);
+    }
+
+    public Iterator<K> keys(){
+        Set<K> set = new HashSet<K>();
+        ContextMap<K, V> cmap = this;
+        do{
+            if(cmap.map!=null)
+                set.addAll(cmap.map.keySet());
+            cmap = cmap.parent;
+        }while(cmap!=null);
+        return set.iterator();
+    }
+}
