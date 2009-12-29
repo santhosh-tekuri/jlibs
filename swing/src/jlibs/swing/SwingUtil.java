9bb7433033a1194896acbcf1c5c124cb80c93d36
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2009-12-29 02:42:05 +0000

    javadoc added

--- swing/src/jlibs/swing/SwingUtil.java
+++ swing/src/jlibs/swing/SwingUtil.java
@@ -16,18 +16,26 @@
 package jlibs.swing;
 
 import javax.swing.*;
-import javax.swing.text.JTextComponent;
 import javax.swing.text.DefaultCaret;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
+ * Various Swing related utilities
+ * 
  * @author Santhosh Kumar T
  */
 public class SwingUtil{
+    /**
+     * sets intial focus in window to the specified component
+     *
+     * @param window    window on which focus has to be set
+     * @param comp      component which need to have initial focus
+     */
     public static void setInitialFocus(Window window, final Component comp){
         window.addWindowFocusListener(new WindowAdapter(){
             @Override
@@ -38,7 +46,12 @@ public class SwingUtil{
         });
     }
 
-    @SuppressWarnings({"UnusedAssignment"})
+    /**
+     * Programmatically perform action on textfield.This does the same
+     * thing as if the user had pressed enter key in textfield.
+     *
+     * @param textField textField on which action to be preformed
+     */
     public static void doAction(JTextField textField){
         String command = null;
         if(textField.getAction()!=null)
@@ -52,6 +65,12 @@ public class SwingUtil{
         }
     }
 
+    /**
+     * sets text of textComp without moving its caret.
+     *
+     * @param textComp  text component whose text needs to be set
+     * @param text      text to be set. null will be treated as empty string
+     */
     public static void setText(JTextComponent textComp, String text){
         if(text==null)
             text = "";
