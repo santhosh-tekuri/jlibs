8ad38b8fe6c5a4bb8e32ce70d81feb8dab544ced
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-01-03 08:32:32 +0000

    made changes to jlaunch.bat to work

--- utils/xsd-viewer.bat
+++ utils/xsd-viewer.bat
@@ -1,9 +1,5 @@
-@ECHO off
-
-set CLASSPATH=3rdparty\annotations.jar;jlibs-core.jar
-set CLASSPATH=%CLASSPATH%;3rdparty\xercesImpl.jar;%CLASSPATH%;3rdparty\jaxen-1.1.1.jar;jlibs-xml.jar
-set CLASSPATH=%CLASSPATH%;3rdparty\org-netbeans-swing-outline.jar;jlibs-swing.jar
-set CLASSPATH=%CLASSPATH%;jlibs-utils.jar
-
-java -classpath %CLASSPATH% jlibs.xml.xsd.XSDOutlinePanelTest
-pause
+@ECHO OFF
+SET APP=%~nx0
+SET APP=%APP:~0,-4%
+CALL ..\launcher\jlaunch.bat %APP%.sh %*
+PAUSE
