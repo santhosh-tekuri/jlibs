1e347306e0b0a57f081d1e58e662caab4122c800
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-01-22 04:43:58 +0000

    - added method to get signature of method - fixed doc related method to handle incomple javadoc - added method to get raw annotation value

--- core/src/jlibs/core/lang/model/ModelUtil.java
+++ core/src/jlibs/core/lang/model/ModelUtil.java
@@ -72,6 +72,27 @@ public class ModelUtil{
         }
     }
 
+    public static String signature(ExecutableElement method, boolean useParamNames){
+        StringBuilder buff = new StringBuilder();
+
+        buff.append(toString(method.getReturnType()));
+        buff.append(' ');
+        buff.append(method.getSimpleName());
+        buff.append('(');
+        int i = 0;
+        for(VariableElement param : method.getParameters()){
+            if(i>0)
+                buff.append(", ");
+            buff.append(toString(param.asType()));
+            if(useParamNames)
+                buff.append(' ').append(param.getSimpleName());
+            i++;
+        }
+        buff.append(')');
+
+        return buff.toString();
+    }
+
     /*-------------------------------------------------[ Annotation ]---------------------------------------------------*/
 
     public static boolean matches(AnnotationMirror mirror, Class annotation){
@@ -86,13 +107,21 @@ public class ModelUtil{
         return null;
     }
 
-    @SuppressWarnings({"unchecked"})
-    public static <T> T getAnnotationValue(Element pos, AnnotationMirror mirror, String method){
+    public static AnnotationValue getRawAnnotationValue(Element pos, AnnotationMirror mirror, String method){
+        for(Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : mirror.getElementValues().entrySet()){
+            if(entry.getKey().getSimpleName().contentEquals(method))
+                return entry.getValue();
+        }
         for(Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : Environment.get().getElementUtils().getElementValuesWithDefaults(mirror).entrySet()){
             if(entry.getKey().getSimpleName().contentEquals(method))
-                return (T)entry.getValue().getValue();
+                return entry.getValue();
         }
         throw new AnnotationError(pos, mirror, "annotation "+((TypeElement)mirror.getAnnotationType().asElement()).getQualifiedName()+" is missing "+method);
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public static <T> T getAnnotationValue(Element pos, AnnotationMirror mirror, String method){
+        return (T)getRawAnnotationValue(pos, mirror, method).getValue();
     }
 
     @SuppressWarnings("unchecked")
@@ -107,15 +136,25 @@ public class ModelUtil{
 
     /*-------------------------------------------------[ javadoc ]---------------------------------------------------*/
 
+    private static final String PARAM = "@param";
+    private static final String RETURN = "@return";
+
     public static String getMethodDoc(String doc){
         if(doc==null)
             return null;
         else{
-            int index = doc.indexOf("@param");
-            if(index==-1)
-                return doc.trim();
-            else
-                return doc.substring(0, index).trim();
+            int index = doc.indexOf(PARAM);
+            if(index==-1){
+                doc = doc.trim();
+            }else
+                doc = doc.substring(0, index).trim();
+
+            index = doc.indexOf(RETURN);
+            if(index==-1){
+                doc = doc.trim();
+            }else
+                doc = doc.substring(0, index).trim();
+            return doc;
         }
     }
 
@@ -131,19 +170,23 @@ public class ModelUtil{
         return new String[]{ str.substring(0, i), str.substring(i) };
     }
 
-    private static final String PARAM = "@param";
     public static Map<String, String> getMethodParamDocs(String doc){
         Map<String, String> docs = new HashMap<String, String>();
-        
-        int index = doc.indexOf(PARAM);
-        doc = doc.substring(index).trim();
-        for(String token: doc.split(PARAM)){
-            token = token.trim();
-            if(token.length()>0){
-                String str[] = split(token, false);
-                str[1] = str[1].trim();
-                if(str[0].length()>0 && str[1].length()>0)
-                    docs.put(str[0], str[1]);
+        if(doc!=null){
+            int index = doc.indexOf(RETURN);
+            if(index!=-1)
+                doc = doc.substring(0, index);
+            index = doc.indexOf(PARAM);
+            if(index!=-1)
+                doc = doc.substring(index).trim();
+            for(String token: doc.split(PARAM)){
+                token = token.trim();
+                if(token.length()>0){
+                    String str[] = split(token, false);
+                    str[1] = str[1].trim();
+                    if(str[0].length()>0 && str[1].length()>0)
+                        docs.put(str[0], str[1]);
+                }
             }
         }
         return docs;
