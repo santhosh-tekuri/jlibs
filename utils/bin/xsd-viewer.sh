5e2425c21b9684c8d8d7e7b2f2501c80fcb556ea
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-02-04 16:26:27 +0000

    including jlauncher and xsd-viewer in distribution

---
+++ utils/bin/xsd-viewer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+`dirname $0`/launcher/jlaunch.sh $0
+exit
+
+#---------------------[ Configuration ]--------------------
+
+<java.classpath>
+../lib/jlibs-core.jar
+
+../lib/external/xercesImpl.jar
+../lib/jlibs-xml.jar
+
+../lib/external/org-netbeans-swing-outline.jar
+../lib/jlibs-swing.jar
+
+../lib/jlibs-utils.jar
+
+<jvm.args>
+-ea
+jlibs.xml.xsd.XSDOutlinePanelTest
+
