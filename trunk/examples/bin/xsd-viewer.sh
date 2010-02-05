86771469167ecbb6f46103f2ff1fe290a7a5a7f7
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-02-05 10:29:10 +0000

    classpaths are extracted into examples.conf

--- trunk/examples/bin/xsd-viewer.sh
+++ trunk/examples/bin/xsd-viewer.sh
@@ -1,21 +1,3 @@
 #!/bin/sh
-`dirname $0`/launcher/jlaunch.sh $0
-exit
 
-#---------------------[ Configuration ]--------------------
-
-<java.classpath>
-../lib/jlibs-core.jar
-
-../lib/external/xercesImpl.jar
-../lib/jlibs-xml.jar
-
-../lib/external/org-netbeans-swing-outline.jar
-../lib/jlibs-swing.jar
-
-../lib/jlibs-examples.jar
-
-<jvm.args>
--ea
-jlibs.xml.xsd.XSDOutlinePanelTest
-
+`dirname $0`/launcher/jlaunch.sh examples.conf jlibs.xml.xsd.XSDOutlinePanelTest $*
