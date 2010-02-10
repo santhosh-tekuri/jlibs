8b343856390cc23ba8da493561bbaa2c50a46f1c
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-02-10 01:36:48 +0000

    working with BOM

--- core/src/main/java/jlibs/core/io/BOM.java
+++ core/src/main/java/jlibs/core/io/BOM.java
@@ -1,34 +1,46 @@
 package jlibs.core.io;
 
 /**
+ * see http://www.w3.org/TR/REC-xml/#sec-guessing-no-ext-info
+ * 
  * @author Santhosh Kumar T
  */
 public enum BOM{
-    UTF8(       new int[]{0xEF, 0xBB, 0xBF}      , null),
-    UCS4_LE(    new int[]{0xFF, 0xFE, 0x00, 0x00}, new int[]{0x3C, 0x00, 0x00, 0x00}),
-    UCS4_BE(    new int[]{0x00, 0x00, 0xFE, 0xFF}, new int[]{0x00, 0x00, 0x00, 0x3C}),
-    UCS4_2143(  new int[]{0x00, 0x00, 0xFF, 0xFE}, new int[]{0x00, 0x00, 0x3C, 0x00}),
-    UCS4_3412(  new int[]{0xFE, 0xFF, 0x00, 0x00}, new int[]{0x00, 0x3C, 0x00, 0x00}),
+    UTF8        ("UTF-8"        , new int[]{0xEF, 0xBB, 0xBF}      , null),
+    UCS4_LE     ("UCS-4LE"      , new int[]{0xFF, 0xFE, 0x00, 0x00}, new int[]{0x3C, 0x00, 0x00, 0x00}),
+    UCS4_BE     ("UCS-4BE"      , new int[]{0x00, 0x00, 0xFE, 0xFF}, new int[]{0x00, 0x00, 0x00, 0x3C}),
+    UCS4_2143   ("UCS-4-2143"   , new int[]{0x00, 0x00, 0xFF, 0xFE}, new int[]{0x00, 0x00, 0x3C, 0x00}),
+    UCS4_3412   ("UCS-4-3412"   , new int[]{0xFE, 0xFF, 0x00, 0x00}, new int[]{0x00, 0x3C, 0x00, 0x00}),
 
-    UTF16_LE(   new int[]{0xFF, 0xFE}            , new int[]{0x3C, 0x00, 0x3F, 0x00}),
-    UTF16_BE(   new int[]{0xFE, 0xFF}            , new int[]{0x00, 0x3C, 0x00, 0x3F}),
-    ASCII(      null                             , new int[]{0x3C, 0x3F, 0x78, 0x6D}),
-    EBCDIC(     null                             , new int[]{0x4C, 0x6F, 0xA7, 0x94}),
+    UTF16_LE    ("UTF-16LE"     , new int[]{0xFF, 0xFE}            , new int[]{0x3C, 0x00, 0x3F, 0x00}),
+    UTF16_BE    ("UTF-16BE"     , new int[]{0xFE, 0xFF}            , new int[]{0x00, 0x3C, 0x00, 0x3F}),
+    ASCII       ("UTF-8"        , null                             , new int[]{0x3C, 0x3F, 0x78, 0x6D}),
+    EBCDIC      ("Cp037"        , null                             , new int[]{0x4C, 0x6F, 0xA7, 0x94}),
     ;
 
-    byte with[];
-    byte without[];
+    private String encoding;
+    private byte with[];
+    private byte without[];
 
-    BOM(int[] with, int[] without){
+    BOM(String encoding, int[] with, int[] without){
+        this.encoding = encoding;
         this.with = toBytes(with);
         this.without = toBytes(without);
     }
 
     private static byte[] toBytes(int[] arr){
-        byte b[] = new byte[arr.length];
-        for(int i=0; i<arr.length; i++)
-            b[i] = (byte)arr[i];
-        return b;
+        if(arr==null)
+            return null;
+        else{
+            byte b[] = new byte[arr.length];
+            for(int i=0; i<arr.length; i++)
+                b[i] = (byte)arr[i];
+            return b;
+        }
+    }
+
+    public String encoding(){
+        return encoding;
     }
 
     public byte[] with(){
