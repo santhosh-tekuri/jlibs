adaec93a2956b8e8f90741104d2f8e8e90618278
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-03-06 12:28:21 +0000

    prettifying javacode in javadoc

--- core/src/main/java/jlibs/core/io/PumpedInputStream.java
+++ core/src/main/java/jlibs/core/io/PumpedInputStream.java
@@ -22,6 +22,62 @@ import java.io.PipedOutputStream;
 import java.io.IOException;
 
 /**
+ * This class simplifies usage of {@link java.io.PipedInputStream} and {@link java.io.PipedOutputStream}
+ * <p>
+ * Using {@link java.io.PipedInputStream} and {@link java.io.PipedOutputStream} looks cumbersome.
+ * <pre class="prettyprint">
+ * PipedInputStream pipedIn = new PipedInputStream();
+ * final PipedOutputStream pipedOut = new PipedOutputStream(pipedIn);
+ * final IOException ioEx[] = { null };
+ * new Thread(){
+ *     &#064;Override
+ *     public void run(){
+ *         try{
+ *             writeDataTo(pipedOut);
+ *             pipedOut.close();
+ *         }catch(IOException ex){
+ *             ioEx[0] = ex;
+ *         }
+ *     }
+ * }.start();
+ * readDataFrom(pipedIn);
+ * pipedIn.close();
+ * if(ioEx[0]!=null)
+ *     throw new RuntimeException("something gone wrong", ioEx[0]);
+ * </pre>
+ * The same can be achieved using {@link PumpedInputStream} as follows:
+ * <pre class="prettyprint">
+ * PumpedInputStream in = new PumpedInputStream(){
+ *     &#064;Override
+ *     protected void {@link #pump(java.io.PipedOutputStream) pump}(PipedOutputStream out) throws Exception{
+ *         writeDataTo(out);
+ *     }
+ * }.{@link #start()}; // start() will spawn new thread
+ * readDataFrom(in);
+ * in.{@link #close()}; // any exceptions occurred in pump(...) are thrown by close()
+ * </pre>
+ *
+ * {@link PumpedInputStream} is an abstract class with following abstract method:
+ * <pre class="prettyprint">
+ * protected abstract void {@link #pump(java.io.PipedOutputStream) pump}(PipedOutputStream out) throws Exception;
+ * </pre>
+ * This method implementation should write data into {@code out} which is passed as argument and close it.<br>
+ * Any exception thrown by {@link #pump(java.io.PipedOutputStream) pump(...)} are wrapped in {@link java.io.IOException} and rethrown by PumpedReader.{@link #close()}.
+ * <p>
+ * {@link PumpedInputStream} implements {@link Runnable} which is supposed to be run in thread.<br>
+ * You can use PumpedInputStream.{@link #start()} method to start thread or spawn thread implicitly.<br>
+ * {@link #start()} method returns self reference.
+ * <pre class="prettyprint">
+ * public PumpedInputStream {@link #start()};
+ * </pre>
+ * The advantage of {@link PumpedInputStream} over {@link java.io.PipedInputStream}/{@link java.io.PipedOutputStream}/{@link Thread} is:
+ * <ul>
+ * <li>it doesn't clutter the exising flow of code</li>
+ * <li>exception handling is better</li>
+ * </ul>
+ *
+ * @see PumpedReader
+ *
  * @author Santhosh Kumar T
  */
 public abstract class PumpedInputStream extends PipedInputStream implements Runnable{
@@ -58,11 +114,25 @@ public abstract class PumpedInputStream extends PipedInputStream implements Runn
         }
     }
 
+    /**
+     * Starts a thread with this instance as runnable.
+     *
+     * @return self reference
+     */
     public PumpedInputStream start(){
         new Thread(this).start();
         return this;
     }
 
+    /**
+     * Closes this stream and releases any system resources
+     * associated with the stream.
+     * <p>
+     * Any exception thrown by {@link #pump(java.io.PipedOutputStream)}
+     * are cached and rethrown by this method
+     *
+     * @exception  IOException  if an I/O error occurs.
+     */
     @Override
     @SuppressWarnings({"ThrowFromFinallyBlock"})
     public void close() throws IOException{
@@ -75,7 +145,9 @@ public abstract class PumpedInputStream extends PipedInputStream implements Runn
     }
 
     /**
-     * write data into <code>out</code>. <code>out</code> can be closed if required.
+     * Subclasse implementation should write data into <code>writer</code>.
+     *
+     * @param out outputstream into which data should be written
      */
     protected abstract void pump(PipedOutputStream out) throws Exception;
 }
--- core/src/main/java/jlibs/core/io/PumpedReader.java
+++ core/src/main/java/jlibs/core/io/PumpedReader.java
@@ -20,6 +20,62 @@ import jlibs.core.lang.ImpossibleException;
 import java.io.*;
 
 /**
+ * This class simplifies usage of {@link java.io.PipedReader} and {@link java.io.PipedWriter}
+ * <p>
+ * Using {@link java.io.PipedReader} and {@link java.io.PipedWriter} looks cumbersome.
+ * <pre class="prettyprint">
+ * PipedReader pipedReader = new PipedReader();
+ * final PipedWriter pipedWriter = new PipedWriter(pipedReader);
+ * final IOException ioEx[] = { null };
+ * new Thread(){
+ *     &#064;Override
+ *     public void run(){
+ *         try{
+ *             writeDataTo(pipedWriter);
+ *             pipedWriter.close();
+ *         }catch(IOException ex){
+ *             ioEx[0] = ex;
+ *         }
+ *     }
+ * }.start();
+ * readDataFrom(pipedReader);
+ * pipedReader.close();
+ * if(ioEx[0]!=null)
+ *     throw new RuntimeException("something gone wrong", ioEx[0]);
+ * </pre>
+ * The same can be achieved using {@link PumpedReader} as follows:
+ * <pre class="prettyprint">
+ * PumpedReader reader = new PumpedReader(){
+ *     &#064;Override
+ *     protected void {@link #pump(java.io.PipedWriter) pump}(PipedWriter writer) throws Exception{
+ *         writeDataTo(writer);
+ *     }
+ * }.{@link #start()}; // start() will spawn new thread
+ * readDataFrom(reader);
+ * reader.{@link #close()}; // any exceptions occurred in pump(...) are thrown by close()
+ * </pre>
+ *
+ * {@link PumpedReader} is an abstract class with following abstract method:
+ * <pre class="prettyprint">
+ * protected abstract void {@link #pump(java.io.PipedWriter) pump}(PipedWriter writer) throws Exception;
+ * </pre>
+ * This method implementation should write data into {@code writer} which is passed as argument and close it.<br>
+ * Any exception thrown by {@link #pump(java.io.PipedWriter) pump(...)} are wrapped in {@link java.io.IOException} and rethrown by PumpedReader.{@link #close()}.
+ * <p>
+ * {@link PumpedReader} implements {@link Runnable} which is supposed to be run in thread.<br>
+ * You can use PumpedReader.{@link #start()} method to start thread or spawn thread implicitly.<br>
+ * {@link #start()} method returns self reference.
+ * <pre class="prettyprint">
+ * public PumpedReader {@link #start()};
+ * </pre>
+ * The advantage of {@link PumpedReader} over {@link java.io.PipedReader}/{@link java.io.PipedWriter}/{@link Thread} is:
+ * <ul>
+ * <li>it doesn't clutter the exising flow of code</li>
+ * <li>exception handling is better</li>
+ * </ul>
+ *
+ * @see PumpedInputStream
+ *
  * @author Santhosh Kumar T
  */
 public abstract class PumpedReader extends PipedReader implements Runnable{
@@ -56,11 +112,25 @@ public abstract class PumpedReader extends PipedReader implements Runnable{
         }
     }
 
+    /**
+     * Starts a thread with this instance as runnable.
+     *
+     * @return self reference
+     */
     public PumpedReader start(){
         new Thread(this).start();
         return this;
     }
 
+    /**
+     * Closes this stream and releases any system resources
+     * associated with the stream.
+     * <p>
+     * Any exception thrown by {@link #pump(java.io.PipedWriter)}
+     * are cached and rethrown by this method
+     *
+     * @exception  IOException  if an I/O error occurs.
+     */
     @Override
     @SuppressWarnings({"ThrowFromFinallyBlock"})
     public void close() throws IOException{
@@ -73,7 +143,9 @@ public abstract class PumpedReader extends PipedReader implements Runnable{
     }
 
     /**
-     * write data into <code>writer</code>. <code>out</code> can be closed if required.
+     * Subclasse implementation should write data into <code>writer</code>.
+     *
+     * @param writer writer into which data should be written
      */
     protected abstract void pump(PipedWriter writer) throws Exception;
 }
