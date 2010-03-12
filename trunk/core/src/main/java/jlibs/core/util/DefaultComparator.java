abfab2a7f29043fb1ed1cd6a1844028a6d2a5e32
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-03-12 08:38:36 +0000

    javadoc added

--- trunk/core/src/main/java/jlibs/core/util/DefaultComparator.java
+++ trunk/core/src/main/java/jlibs/core/util/DefaultComparator.java
@@ -20,12 +20,53 @@ import jlibs.core.lang.NotImplementedException;
 import java.util.Comparator;
 
 /**
+ * This is default implementation of {@link Comparator}, which can compare 
+ * objects implementing {@link Comparable}.
+ * <p>
+ * <b>Null-Friendly:</b>
+ * <p>
+ * {@code null} is treated less than {@code non-null}.
+ * <pre class="prettyprint">
+ * Comparator<String> comp = new DefaultComparator<String>();
+ * System.out.println(comp.compareTo(null, "helloworld")); // prints -1
+ * System.out.println(comp.compare("helloworld", null)); // prints 1
+ * System.out.println(comp.compare(null, null)); // prints 0
+ * </pre>
+ * 
+ * If the objects compared doesn't implement {@link Comparable}, it throws
+ * {@link NotImplementedException}.<br>
+ * You can extend {@link DefaultComparator} for any objects that doesn't implement {@link Comparable}.<br>
+ * For Example:
+ * <pre class="prettyprint">
+ * class Employee{
+ *     int marks;
+ *     int age;
+ * 
+ *     public Employee(int marks, int age){
+ *         this.marks = marks;
+ *         this.age = age;
+ *     }
+ * }
+ * 
+ * class EmployeeAgeComparator extends DefaultComparator<Employee>{
+ *     &#064;Override
+ *     protected int _compare(Employee emp1, Employee emp2){
+ *         return emp1.age - emp2.age;
+ *     }
+ * } 
+ * </pre>
+ * 
+ * the {@link #compare(Object, Object) compare(...)} method in {@link DefaultComparator} is final. 
+ * This method takes care of comparing values involving {@code nulls}. If both arguments are {@code non-null},
+ * then it delegates the comparison to {@link #_compare(Object, Object) _compare(...)}
+ * <p>
+ * So it is guaranteed that, both arguments of {@link #_compare(Object, Object) _compare(...)} are {@code non-null}.
+ *  
  * @author Santhosh Kumar T
  */
 public class DefaultComparator<T> implements Comparator<T>{
-
     /**
-     * this method can handle nulls ( null<non-null )
+     * this method can handle nulls ( null&lt;non-null )
      */
     @Override
     public final int compare(T o1, T o2){
@@ -40,7 +81,7 @@ public class DefaultComparator<T> implements Comparator<T>{
     }
 
     /**
-     * params o1 and o2 will be non-null
+     * Arguments {@code o1} and {@code o2} will be non-null
      */
     @SuppressWarnings({"unchecked"})
     protected int _compare(T o1, T o2){
