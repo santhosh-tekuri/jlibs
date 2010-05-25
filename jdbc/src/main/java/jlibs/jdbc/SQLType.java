b608cf97974fe9235490d24dab9e9cf934aeedce
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-05-25 21:11:17 +0000

    Type enums

---
+++ jdbc/src/main/java/jlibs/jdbc/SQLType.java
@@ -0,0 +1,67 @@
+/**
+ * JLibs: Common Utilities for Java
+ * Copyright (C) 2009  Santhosh Kumar T <santhosh.tekuri@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package jlibs.jdbc;
+
+import java.sql.Types;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public enum SQLType{
+	BIT             (Types.BIT),
+	TINYINT         (Types.TINYINT),
+	SMALLINT        (Types.SMALLINT),
+	INTEGER         (Types.INTEGER),
+	BIGINT          (Types.BIGINT),
+	FLOAT           (Types.FLOAT),
+	REAL            (Types.REAL),
+	DOUBLE          (Types.DOUBLE),
+	NUMERIC         (Types.NUMERIC),
+	DECIMAL         (Types.DECIMAL),
+	CHAR            (Types.CHAR),
+	VARCHAR         (Types.VARCHAR),
+	LONGVARCHAR     (Types.LONGNVARCHAR),
+	DATE            (Types.DATE),
+	TIME            (Types.TIME),
+	TIMESTAMP       (Types.TIMESTAMP),
+	BINARY          (Types.BINARY),
+	VARBINARY       (Types.VARBINARY),
+	LONGVARBINARY   (Types.LONGVARBINARY),
+    BLOB            (Types.BLOB),
+    CLOB            (Types.CLOB),
+    BOOLEAN         (Types.BOOLEAN)
+
+//	NULL		=   0;
+//	OTHER		= 1111;
+//    JAVA_OBJECT         = 2000;
+//    DISTINCT            = 2001;
+//    STRUCT              = 2002;
+//    ARRAY               = 2003;
+//    REF                 = 2006;
+//    DATALINK = 70;
+//    ROWID = -8;
+//    NCHAR = -15;
+//    NVARCHAR = -9;
+//    LONGNVARCHAR = -16;
+//    NCLOB = 2011;
+//    SQLXML = 2009;
+    ;
+    public final int type;
+
+    SQLType(int type){
+        this.type = type;
+    }
+}
