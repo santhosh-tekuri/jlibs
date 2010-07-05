5739c00b9364ec2c5f0cc9ebf12c88729bb81928
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-07-05 17:35:56 +0000

    property name added to column

--- trunk/jdbc/src/main/java/jlibs/jdbc/ColumnMetaData.java
+++ trunk/jdbc/src/main/java/jlibs/jdbc/ColumnMetaData.java
@@ -19,12 +19,14 @@ package jlibs.jdbc;
  * @author Santhosh Kumar T
  */
 public class ColumnMetaData{
+    public final String property;
     public final String name;
     public final JavaType javaType;
     public final SQLType sqlType;
     public final boolean primary;
     public final boolean auto;
-    public ColumnMetaData(String name, JavaType javaType, SQLType sqlType, boolean primary, boolean auto){
+    public ColumnMetaData(String property, String name, JavaType javaType, SQLType sqlType, boolean primary, boolean auto){
+        this.property = property;
         this.name = name;
         this.javaType = javaType;
         this.sqlType = sqlType;
