9d4d438396af5d63fb72b511e07ae1b5ec016a0f
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-08-09 09:05:59 +0000

    moved dao generation logic from TableAnnotationProcessor into Columns class

--- jdbc/src/main/java/jlibs/jdbc/annotations/processor/Columns.java
+++ jdbc/src/main/java/jlibs/jdbc/annotations/processor/Columns.java
@@ -17,15 +17,29 @@ package jlibs.jdbc.annotations.processor;
 
 import jlibs.core.annotation.processing.AnnotationError;
 import jlibs.core.annotation.processing.Printer;
+import jlibs.core.lang.ImpossibleException;
 import jlibs.core.lang.Noun;
 import jlibs.core.lang.StringUtil;
 import jlibs.core.lang.model.ModelUtil;
+import jlibs.jdbc.DAO;
+import jlibs.jdbc.JavaType;
 import jlibs.jdbc.SQLType;
+import jlibs.jdbc.annotations.Column;
 import jlibs.jdbc.annotations.Table;
 
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.ElementFilter;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static jlibs.core.annotation.processing.Printer.MINUS;
 import static jlibs.core.annotation.processing.Printer.PLUS;
@@ -34,10 +48,13 @@ import static jlibs.core.annotation.processing.Printer.PLUS;
  * @author Santhosh Kumar T
  */
 class Columns extends ArrayList<ColumnProperty>{
+    public static Map<TypeElement, Columns> ALL = new HashMap<TypeElement, Columns>();
+    
     public final String tableName;
     public final TypeElement tableClass;
 
     public Columns(TypeElement clazz){
+        ALL.put(clazz, this);
         tableClass = clazz;
         String tableName = ModelUtil.getAnnotationValue(clazz, Table.class, "name");
         if(tableName.length()==0)
@@ -90,6 +107,54 @@ class Columns extends ArrayList<ColumnProperty>{
 
         columnProperty.validateType();
         return super.add(columnProperty);
+    }
+
+    public void process(TypeElement c){
+        for(ExecutableElement method: ElementFilter.methodsIn(c.getEnclosedElements())){
+            AnnotationMirror mirror = ModelUtil.getAnnotationMirror(method, Column.class);
+            if(mirror!=null){
+                if(!ModelUtil.isAccessible(method, true, false))
+                    throw new AnnotationError(method, "Invalid access modifier used on method with @Column");
+                add(new MethodColumnProperty(method, mirror));
+            }
+        }
+        for(VariableElement element: ElementFilter.fieldsIn(c.getEnclosedElements())){
+            AnnotationMirror mirror = ModelUtil.getAnnotationMirror(element, Column.class);
+            if(mirror!=null){
+                if(!ModelUtil.isAccessible(element, true, false))
+                    throw new AnnotationError(element, "Invalid access modifier used on field with @Column");
+                add(new FieldColumnProperty(element, mirror));
+            }
+        }
+    }
+
+    public void generateConstructor(Printer printer){
+        printer.printlns(
+            "public "+printer.generatedClazz+"(DataSource dataSource){",
+                PLUS,
+                "super(dataSource, new TableMetaData(\""+StringUtil.toLiteral(tableName, false)+"\",",
+                    PLUS
+        );
+        int i = 0;
+        for(ColumnProperty column: this){
+            printer.println(
+                    "new ColumnMetaData(" ,
+                    "\""+StringUtil.toLiteral(column.propertyName(), false)+"\", ",
+                    "\""+StringUtil.toLiteral(column.columnName(), false)+"\", ",
+                    JavaType.class.getSimpleName()+'.'+column.javaType().name()+", ",
+                    SQLType.class.getSimpleName()+'.'+column.sqlType().name()+", ",
+                    column.primary()+", ",
+                    column.auto()+")",
+                    (i==size()-1 ? "" : ",")
+            );
+            i++;
+        }
+        printer.printlns(
+                    MINUS,
+                "));",
+                MINUS,
+            "}"
+        );
     }
 
     private void addDefaultCase(Printer printer){
@@ -163,6 +228,116 @@ class Columns extends ArrayList<ColumnProperty>{
                 String constant = StringUtil.underscore(column.propertyName()).toUpperCase();
                 printer.println("public static final "+typeMapperQName+" TYPE_MAPPER_"+constant+" = new "+typeMapperQName+"();");
             }
+        }
+    }
+
+    private void generateNewRow(Printer printer){
+        printer.printlns(
+            "@Override",
+            "public "+printer.clazz.getSimpleName()+" newRow(){",
+                PLUS,
+                "return new "+printer.clazz.getSimpleName()+"();",
+                MINUS,
+            "}"
+        );
+    }
+
+    private void generateNewRecord(Printer printer){
+        printer.printlns(
+            "@Override",
+            "public "+printer.clazz.getSimpleName()+" newRecord(ResultSet rs) throws SQLException{",
+                PLUS,
+                printer.clazz.getSimpleName()+" __record = newRow();"
+        );
+        int i = 1;
+        for(ColumnProperty column: this){
+            String code[] = column.getValueFromResultSet(i);
+            if(code.length>1)
+                printer.println(code[0]);
+            String value = code[code.length-1];
+            printer.println("setColumnValue("+(i-1)+", __record, "+value+");");
+            i++;
+        }
+        printer.printlns(
+                "return __record;",
+                MINUS,
+            "}"
+        );
+    }
+
+    private void generateGetAutoColumnValue(Printer printer){
+        printer.printlns(
+            "@Override",
+            "public Object getAutoColumnValue(ResultSet rs) throws SQLException{",
+                PLUS
+        );
+        if(autoColumn==-1)
+            printer.println("throw new "+ ImpossibleException.class.getName()+"();");
+        else{
+            ColumnProperty column = get(autoColumn);
+            String code[] = column.getValueFromResultSet(1);
+            if(code.length>1)
+                printer.println(code[0]);
+            printer.println("return "+code[code.length-1]+';');
+        }
+        printer.printlns(
+                MINUS,
+            "}"
+        );
+    }
+
+    private void generateDAO(Printer printer){
+        printer.printPackage();
+
+        printer.importClass(ImpossibleException.class);
+        printer.importPackage(DAO.class);
+        printer.importPackage(Connection.class);
+        printer.importClass(DataSource.class);
+        printer.println();
+
+        printer.printClassDoc();
+        TypeElement extendClass = (TypeElement)((DeclaredType)ModelUtil.getAnnotationValue(printer.clazz, Table.class, "extend")).asElement();
+        printer.print("public class "+printer.generatedClazz +" extends "+extendClass.getQualifiedName());
+        if(extendClass.getQualifiedName().contentEquals(DAO.class.getName()))
+            printer.println("<"+printer.clazz.getSimpleName()+'>');
+        printer.println("{");
+        printer.indent++;
+
+        generateTypeMapperConstants(printer);
+        printer.println();
+        generateConstructor(printer);
+        printer.println();
+        generateNewRow(printer);
+        printer.println();
+        generateNewRecord(printer);
+        printer.println();
+        generateGetAutoColumnValue(printer);
+        printer.println();
+
+        generateGetColumnValue(printer);
+        printer.println();
+        generateSetColumnValue(printer);
+
+        for(ExecutableElement method: ElementFilter.methodsIn(extendClass.getEnclosedElements())){
+            DMLMethod dmlMethod = DMLMethod.create(printer, method, this);
+            if(dmlMethod!=null)
+                dmlMethod.generate();
+        }
+
+        printer.indent--;
+        printer.println("}");
+    }
+
+    public void generateDAO(){
+        Printer pw = null;
+        try{
+            pw = Printer.get(tableClass, Table.class, TableAnnotationProcessor.FORMAT);
+            generateDAO(pw);
+        }catch(IOException ex){
+            throw new RuntimeException(ex);
+        }finally{
+            if(pw!=null)
+                pw.close();
         }
     }
 }
