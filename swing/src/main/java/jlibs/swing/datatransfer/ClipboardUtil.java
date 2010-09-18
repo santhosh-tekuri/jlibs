ea96c2f159e57d6d51405d38d69f3d6b20cbe50c
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-09-18 15:03:24 +0000

    Clipboard related utilities

--- swing/src/main/java/jlibs/swing/datatransfer/ClipboardUtil.java
+++ swing/src/main/java/jlibs/swing/datatransfer/ClipboardUtil.java
@@ -1,3 +1,18 @@
+/**
+ * JLibs: Common Utilities for Java
+ * Copyright (C) 2009  Santhosh Kumar T <santhosh.tekuri@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
 package jlibs.swing.datatransfer;
 
 import jlibs.core.lang.ImpossibleException;
@@ -27,8 +42,7 @@ public class ClipboardUtil{
             clear();
         else{
             StringSelection selection = new StringSelection(text);
-            Clipboard clipboard = clipboard();
-            clipboard.setContents(selection, selection);
+            clipboard().setContents(selection, selection);
         }
     }
 
