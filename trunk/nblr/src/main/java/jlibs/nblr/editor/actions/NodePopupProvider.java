1402415f5740bdd6220ca500234e70528ed22557
santhosh.tekuri <santhosh.tekuri@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-09-20 12:16:41 +0000

    action to delete sink node

--- trunk/nblr/src/main/java/jlibs/nblr/editor/actions/NodePopupProvider.java
+++ trunk/nblr/src/main/java/jlibs/nblr/editor/actions/NodePopupProvider.java
@@ -16,12 +16,14 @@
 package jlibs.nblr.editor.actions;
 
 import jlibs.nblr.editor.RuleScene;
+import jlibs.nblr.rules.Edge;
 import jlibs.nblr.rules.Node;
 import org.netbeans.api.visual.action.PopupMenuProvider;
 import org.netbeans.api.visual.widget.Widget;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 /**
  * @author Santhosh Kumar T
@@ -33,8 +35,9 @@ public class NodePopupProvider implements PopupMenuProvider{
         this.scene = scene;
     }
 
+    private Node node;
     public JPopupMenu getPopupMenu(Widget widget, Point localLocation){
-        final Node node = (Node)scene.findObject(widget);
+        node = (Node)scene.findObject(widget);
 
         JMenu insertNodeMenu = new JMenu("Insert Node");
         insertNodeMenu.add(new InsertBeforeNodeAction(scene, node));
@@ -57,7 +60,32 @@ public class NodePopupProvider implements PopupMenuProvider{
         popup.add(insertNodeMenu);
         popup.add(insertStringMenu);
         popup.add(actionMenu);
+        popup.addSeparator();
+        popup.add(new ChoiceAction("Delete", deleteSinkAction));
 
         return popup;
     }
+    
+    private Action deleteSinkAction = new AbstractAction(){
+        @Override
+        public void actionPerformed(ActionEvent ae){
+            for(Edge edge: node.incoming()){
+                edge.setSource(null);
+                edge.setTarget(null);
+            }
+            scene.refresh();
+        }
+
+        @Override
+        public boolean isEnabled(){
+            if(scene.getRule().node==node)
+                return false;
+            
+            for(Edge edge: node.outgoing){
+                if(!edge.loop())
+                    return false;
+            }
+            return true;
+        }
+    };
 }
