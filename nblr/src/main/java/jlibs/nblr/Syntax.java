d0b671c51cad435a38b3c9b2c700eebdcca06269
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-09-20 23:15:02 +0000

    removed main() method

--- nblr/src/main/java/jlibs/nblr/Syntax.java
+++ nblr/src/main/java/jlibs/nblr/Syntax.java
@@ -15,23 +15,15 @@
 
 package jlibs.nblr;
 
-import jlibs.core.annotation.processing.Printer;
-import jlibs.nblr.codegen.java.JavaCodeGenerator;
 import jlibs.nblr.matchers.Matcher;
 import jlibs.nblr.rules.Rule;
 import jlibs.xml.sax.SAXProducer;
 import jlibs.xml.sax.XMLDocument;
-import jlibs.xml.sax.binding.BindingHandler;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.namespace.QName;
-import javax.xml.transform.stream.StreamResult;
-import java.io.PrintWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
-import static jlibs.nblr.matchers.Matcher.*;
 
 /**
  * @author Santhosh Kumar T
@@ -62,41 +54,5 @@ public class Syntax implements SAXProducer{
         for(Rule rule: rules.values())
             xml.add(rule);
         xml.endElement();
-    }
-
-    /*-------------------------------------------------[ Test ]---------------------------------------------------*/
-    
-    public static void main(String[] args) throws Exception{
-        Syntax syntax = new Syntax();
-
-        Matcher GT              = syntax.add("GT",             ch('>'));
-        Matcher BRACKET_CLOSE   = syntax.add("BRACKET_CLOSE",  ch(']'));
-        Matcher Q               = syntax.add("Q",              ch('\''));
-        Matcher DQ              = syntax.add("DQ",             ch('"'));
-        Matcher DIGIT           = syntax.add("DIGIT",          range("0-9"));
-        Matcher WS              = syntax.add("WS",             any(" \t\n\r"));
-        Matcher ENCODING_START  = syntax.add("ENCODING_START", or(range("A-Z"), range("a-z")));
-        Matcher ENCODING_PART   = syntax.add("ENCODING_PART",  or(ENCODING_START, DIGIT, any("._-")));
-        Matcher CHAR            = syntax.add("CHAR",           or(any("\t\n\r"), range(" -\uD7FF"), range("\uE000-\uFFFD")/*, range("\u10000-\u10FFFF")*/));
-        Matcher DASH            = syntax.add("DASH",           ch('-'));
-        Matcher NDASH           = syntax.add("NDASH",          minus(CHAR, ch('-')));
-
-        Printer printer = new Printer(new PrintWriter(System.out, true));
-        new JavaCodeGenerator(syntax, printer).generateCode();
-        
-        String file = "syntax.xml";
-
-        XMLDocument xml = new XMLDocument(new StreamResult(file), true, 4, null);
-        xml.startDocument();
-        xml.add(syntax);
-        xml.endDocument();
-
-        BindingHandler handler = new BindingHandler(SyntaxBinding.class);
-        Syntax newSyntax = (Syntax)handler.parse(new InputSource(file));
-
-        xml = new XMLDocument(new StreamResult(System.out), true, 4, null);
-        xml.startDocument();
-        xml.add(newSyntax);
-        xml.endDocument();
     }
 }
