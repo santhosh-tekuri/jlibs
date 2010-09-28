790ad9f853c9f3f3aa660910c84f433f37bce1e8
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-09-28 16:11:05 +0000

    resolved mvn license warning

--- nblr/src/main/java/jlibs/nblr/rules/RuleTarget.java
+++ nblr/src/main/java/jlibs/nblr/rules/RuleTarget.java
@@ -12,6 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  */
+
 package jlibs.nblr.rules;
 
 import jlibs.xml.sax.SAXProducer;
