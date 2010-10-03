c0d23ec242f23608d25903a8e02ad9ba31dd8e2e
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-10-03 05:33:00 +0000

    avoid exception masking

--- trunk/core/src/main/java/jlibs/core/io/IOUtil.java
+++ trunk/core/src/main/java/jlibs/core/io/IOUtil.java
@@ -112,18 +112,33 @@ public class IOUtil{
     public static <T extends OutputStream> T pump(InputStream is, T os, boolean closeIn, boolean closeOut) throws IOException{
         byte buff[] = new byte[1024];
         int len;
+        Exception exception = null;
         try{
             while((len=is.read(buff))!=-1)
                 os.write(buff, 0, len);
+        }catch(Exception ex){
+            exception = ex;
         }finally{
             try{
-                if(closeIn)
-                    is.close();
-            }finally{
-                if(closeOut)
-                    os.close();
+                try{
+                    if(closeIn)
+                        is.close();
+                }finally{
+                    if(closeOut)
+                        os.close();
+                }
+            }catch(IOException ex){
+                if(exception!=null)
+                    ex.printStackTrace();
+                else
+                    exception = ex;
             }
         }
+
+        if(exception instanceof IOException)
+            throw (IOException)exception;
+        else if(exception instanceof RuntimeException)
+            throw (RuntimeException)exception;
         return os;
     }
 
@@ -154,18 +169,34 @@ public class IOUtil{
     public static <T extends Writer> T pump(Reader reader, T writer, boolean closeReader, boolean closeWriter) throws IOException{
         char buff[] = new char[1024];
         int len;
+        Exception exception = null;
         try{
             while((len=reader.read(buff))!=-1)
                 writer.write(buff, 0, len);
+        }catch(Exception ex){
+            exception = ex;
         }finally{
             try{
-                if(closeReader)
-                    reader.close();
-            }finally{
-                if(closeWriter)
-                    writer.close();
+                try{
+                    if(closeReader)
+                        reader.close();
+                }finally{
+                    if(closeWriter)
+                        writer.close();
+                }
+            }catch(IOException ex){
+                if(exception!=null)
+                    ex.printStackTrace();
+                else
+                    exception = ex;
             }
         }
+
+        if(exception instanceof IOException)
+            throw (IOException)exception;
+        else if(exception instanceof RuntimeException)
+            throw (RuntimeException)exception;
+
         return writer;
     }
 
