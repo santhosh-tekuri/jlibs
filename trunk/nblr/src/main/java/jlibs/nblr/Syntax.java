82f14135e97bd6d9590f84b4ffca061ee22c8d75
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2010-10-18 18:39:22 +0000

    saveTo and loadFrom methods added to Syntax

--- trunk/nblr/src/main/java/jlibs/nblr/Syntax.java
+++ trunk/nblr/src/main/java/jlibs/nblr/Syntax.java
@@ -17,11 +17,22 @@ package jlibs.nblr;
 
 import jlibs.nblr.actions.EventAction;
 import jlibs.nblr.actions.PublishAction;
+import jlibs.nblr.editor.serialize.SyntaxBinding;
+import jlibs.nblr.editor.serialize.SyntaxDocument;
 import jlibs.nblr.matchers.Matcher;
 import jlibs.nblr.rules.Edge;
 import jlibs.nblr.rules.Node;
 import jlibs.nblr.rules.Rule;
+import jlibs.xml.sax.binding.BindingHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -49,7 +60,7 @@ public class Syntax{
         updateRuleIDs();
     }
     
-    private void updateRuleIDs(){
+    public void updateRuleIDs(){
         int id = 0;
         for(Rule r: rules.values())
             r.id = id++;
@@ -67,16 +78,43 @@ public class Syntax{
     public List<Rule> usages(Rule rule){
         List<Rule> usages = new ArrayList<Rule>();
         for(Rule r: rules.values()){
-            if(r!=rule){
-                for(Edge edge: r.edges()){
-                    if(edge.ruleTarget!=null){
-                        if(edge.ruleTarget.rule==rule && !usages.contains(r))
-                            usages.add(r);
-                    }
+            for(Edge edge: r.edges()){
+                if(edge.ruleTarget!=null){
+                    if(edge.ruleTarget.rule==rule && !usages.contains(r))
+                        usages.add(r);
                 }
             }
         }
         return usages;
+    }
+
+    /*-------------------------------------------------[ Serialization ]---------------------------------------------------*/
+
+    public void saveTo(File file) throws IOException{
+        FileOutputStream fout = null;
+        try{
+            fout = new FileOutputStream(file);
+            SyntaxDocument xml = new SyntaxDocument(new StreamResult(fout));
+            xml.startDocument();
+            xml.add(this);
+            xml.endDocument();
+        }catch(IOException ex){
+            throw ex;
+        }catch(Exception ex){
+            throw new IOException(ex);
+        }finally{
+            try{
+                if(fout!=null)
+                    fout.close();
+            }catch(IOException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static Syntax loadFrom(File file) throws IOException, SAXException, ParserConfigurationException{
+        BindingHandler handler = new BindingHandler(SyntaxBinding.class);
+        return (Syntax)handler.parse(new InputSource(new FileInputStream(file)));
     }
 
     /*-------------------------------------------------[ Actions ]---------------------------------------------------*/
@@ -102,4 +140,6 @@ public class Syntax{
         }
         return methods;
     }
+
+
 }
