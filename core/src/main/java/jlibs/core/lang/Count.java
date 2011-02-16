45fa63b35e1e427f54051ef7a9615f528efcf394
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2011-02-16 05:11:54 +0000

    Counting by Unit

---
+++ core/src/main/java/jlibs/core/lang/Count.java
@@ -0,0 +1,129 @@
+/**
+ * JLibs: Common Utilities for Java
+ * Copyright (C) 2009  Santhosh Kumar T <santhosh.tekuri@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package jlibs.core.lang;
+
+import java.util.Arrays;
+
+/**
+ * Class used to count by units
+ *
+ * @author Santhosh Kumar T
+ */
+public class Count<T extends Count.Unit> implements Comparable<Count<T>>{
+    private long amounts[];
+    private T units[];
+
+    private Count(T units[]){
+        this.units = units;
+        amounts = new long[units.length];
+    }
+
+    static interface Unit{
+        public int ordinal();
+        public int count();
+        public String toString();
+    }
+
+    public static <T extends Unit> Count<T> newInstance(Class<T> unitClass){
+        if(!unitClass.isEnum())
+            throw new IllegalArgumentException(unitClass+" should be enum");
+        return new Count<T>(unitClass.getEnumConstants());
+    }
+
+    /*-------------------------------------------------[ Manipulation ]---------------------------------------------------*/
+
+    public Count<T> add(long amount, T unit){
+        amounts[unit.ordinal()] += amount;
+        for(int i=unit.ordinal(); i<units.length-1; i++){
+            if(amounts[i]>=units[i].count()){
+                amounts[i+1] += amounts[i] / units[i].count();
+                amounts[i] %= units[i].count();
+            }else
+                break;
+        }
+        return this;
+    }
+
+    public Count<T> add(Count<T> other){
+        for(T unit: units)
+            add(other.amounts[unit.ordinal()], unit);
+        return this;
+    }
+
+    /*-------------------------------------------------[ Getters ]---------------------------------------------------*/
+
+    public long get(T unit){
+        return amounts[unit.ordinal()];
+    }
+
+    public strictfp double to(T unit){
+        long value = amounts[unit.ordinal()];
+        for(int i=units.length-2; i>=unit.ordinal(); i--)
+            value = amounts[i]+ units[i].count()*amounts[i+1];
+        if(unit.ordinal()>0){
+            double before = amounts[0];
+            for(int i=1; i<unit.ordinal(); i++)
+                before = before/units[i-1].count() + amounts[i];
+            return value + before/units[unit.ordinal()-1].count();
+        }
+        return value;
+    }
+
+    /*-------------------------------------------------[ Object ]---------------------------------------------------*/
+
+    @Override
+    public int hashCode(){
+        long hash = 0;
+        for(long amount: amounts)
+            hash =31*hash + amount;
+        return (int)(hash ^ hash>>>32);
+    }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof Count){
+            Count<?> that = (Count<?>)obj;
+            return Arrays.equals(this.units, that.units) && Arrays.equals(this.amounts, that.amounts);
+        }
+        return false;
+    }
+
+    public String toString(){
+        StringBuilder buff = new StringBuilder();
+
+        for(int i=units.length-1; i>=0; i--){
+            if(amounts[i]>0){
+                if(buff.length()>0)
+                    buff.append(' ');
+                buff.append(amounts[i]);
+                buff.append(' ');
+                buff.append(units[i]);
+            }
+        }
+        return buff.length()==0 ? "0" : buff.toString();
+    }
+
+    /*-------------------------------------------------[ Comparable ]---------------------------------------------------*/
+
+    @Override
+    public int compareTo(Count<T> that){
+        for(int i=units.length-1; i>=0; i--){
+            if(this.amounts[i]!=that.amounts[i])
+                return this.amounts[i]<that.amounts[i] ? -1 : +1;
+        }
+        return 0;
+    }
+}
