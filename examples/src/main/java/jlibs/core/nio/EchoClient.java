4c0022babb9c331f933a76440e32462eeaf2e504
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2011-03-06 01:56:29 +0000

    selector.invokeXXX methods now wakeup selector

--- examples/src/main/java/jlibs/core/nio/EchoClient.java
+++ examples/src/main/java/jlibs/core/nio/EchoClient.java
@@ -80,7 +80,6 @@ public class EchoClient{
                                     }
                                 }
                             });
-                            selector.wakeup();
                             inBuffer.wait();
                         }
                     }catch(Exception ex){
