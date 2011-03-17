666973dfd95278994dbf25b24f4d13647155dda5
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2011-03-17 06:42:32 +0000

    clinet pooling per selector

--- trunk/core/src/main/java/jlibs/core/nio/ClientPool.java
+++ trunk/core/src/main/java/jlibs/core/nio/ClientPool.java
@@ -15,102 +15,155 @@
 
 package jlibs.core.nio;
 
+import jlibs.core.util.NonNullIterator;
+
 import java.net.SocketAddress;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * @author Santhosh Kumar T
  */
-public class ClientPool extends AttachmentSupport{
-    private final SocketAddress remote;
+public class ClientPool extends AttachmentSupport implements Iterable<ClientPool.Entry>{
+    private final NIOSelector selector;
+    private final Map<SocketAddress, Entry> map = new HashMap<SocketAddress, Entry>();
 
-    public ClientPool(SocketAddress remote){
-        this.remote = remote;
-    }
-
-    public SocketAddress remoteAddress(){
-        return remote;
+    protected ClientPool(NIOSelector selector){
+        this.selector = selector;
     }
 
     public String toString(){
-        return "ClientPool["+remote+"]";
+        return "ClientPool@"+selector.id;
     }
 
-    // should be called in client's thread
-    public synchronized void add(ClientChannel client, long timeout){
-        if(client.pool!=null || client.futurePool!=null)
-            throw new IllegalArgumentException("client is already pooled");
+    protected void add(ClientChannel client, long timeout){
+        if(timeout<=0)
+            throw new IllegalArgumentException("invalid timeout: "+timeout);
+        if(client.poolFlag>=0)
+            throw new IllegalArgumentException(client+" is already pooled");
+        if(!client.realChannel().isConnected())
+            throw new IllegalArgumentException(client+" is not connected yet");
         if(client.interests()!=0)
-            throw new IllegalArgumentException("client.interests() should be zero");
-        if(client.sslEnabled() && client.plainTransport().interests()!=0){
-            client.futurePool = this;
-            client.futurePoolTimeout = timeout;
-            addToList(client);
-        }else
-            _add(client, timeout);
+            throw new IllegalArgumentException(client+".interests() should be zero");
+
+        client.poolFlag = timeout;
+        if(client.key.interestOps()==0)
+            track(client);
+        addToList(client);
     }
 
-    protected synchronized void _add(ClientChannel client, long timeout){
-        client.pool = this;
+    protected void track(ClientChannel client){
         long oldTimeout = client.getTimeout();
-        client.setTimeout(timeout);
+        client.setTimeout(client.poolFlag);
         client.nioSelector.timeoutTracker.track(client);
         client.setTimeout(oldTimeout);
-
-        if(client.futurePool!=null)
-            client.futurePool = null;
-        else
-            addToList(client);
+        client.poolFlag = 0;
     }
 
-    public synchronized ClientChannel remove(){
-        final ClientChannel client = size==0 ? null : head.next;
-        if(client!=null){
-            removeFromList(client);
-            client.nioSelector.invokeLater(new Runnable(){
-                @Override
-                public void run(){
-                    remove(client);
-                }
-            });
-        }
-        return client;
+    public ClientChannel remove(SocketAddress remote){
+        if(size==0)
+            return null;
+        Entry list = map.get(remote);
+        if(list==null)
+            return null;
+        return list.remove();
     }
 
-    // should be called in client's thread
-    public synchronized boolean remove(ClientChannel client){
-        if(client.pool!=this && client.futurePool!=this)
-            throw new IllegalArgumentException("This client doesn't belong to this pool");
-        client.pool = client.futurePool = null;
-        return removeFromList(client);
+    protected boolean remove(ClientChannel client){
+        if(client.poolFlag<0)
+            return false;
+        SocketAddress address = client.realChannel().socket().getRemoteSocketAddress();
+        Entry list = map.get(address);
+        list.remove(client);
+        return true;
     }
 
     /*-------------------------------------------------[ LinkedList ]---------------------------------------------------*/
 
-    private ClientChannel head = new ClientChannel();
-    private int size;
+    public class Entry{
+        private SocketAddress address;
+        private ClientChannel head = new ClientChannel();
+        private int size;
 
-    public synchronized int size(){
-        return size;
-    }
+        protected Entry(SocketAddress address){
+            this.address = address;
+        }
 
-    private void addToList(ClientChannel channel){
-        assert channel.prev==null && channel.next==null;
+        public SocketAddress address(){
+            return address;
+        }
 
-        channel.prev = head.prev;
-        channel.next = head;
-        channel.prev.next = channel;
-        channel.next.prev = channel;
-        size++;
-    }
+        public int size(){
+            return size;
+        }
 
-    private boolean removeFromList(ClientChannel channel){
-        if(channel.prev!=null){ // present in list
+        protected void add(ClientChannel channel){
+            assert channel.prev==null && channel.next==null;
+
+            channel.prev = head.prev;
+            channel.next = head;
+            channel.prev.next = channel;
+            channel.next.prev = channel;
+            size++;
+            ClientPool.this.size++;
+        }
+
+        protected void remove(ClientChannel channel){
+            if(channel.poolFlag==0)
+                channel.nioSelector.timeoutTracker.untrack(channel);
+            channel.poolFlag = -1;
+
             channel.prev.next = channel.next;
             channel.next.prev = channel.prev;
             channel.prev = channel.next = null;
             size--;
-            return true;
-        }else
-            return false;
+            ClientPool.this.size--;
+
+            if(size()==0)
+                map.remove(address);
+        }
+
+        public ClientChannel remove(){
+            if(size==0)
+                return null;
+            ClientChannel client = head.next;
+            remove(client);
+            return client;
+        }
+
+        @Override
+        public String toString(){
+            return address+"="+size;
+        }
+    }
+
+    @Override
+    public Iterator<Entry> iterator(){
+        final Iterator<Entry> entries = map.values().iterator();
+        return new NonNullIterator<Entry>(){
+            @Override
+            protected Entry findNext(){
+                return entries.next();
+            }
+        };
+    }
+
+    private int size;
+    public int size(){
+        return size;
+    }
+
+    public int size(SocketAddress remote){
+        Entry list = map.get(remote);
+        return list==null ? 0 : list.size();
+    }
+
+    private void addToList(ClientChannel channel){
+        SocketAddress address = channel.realChannel().socket().getRemoteSocketAddress();
+        Entry list = map.get(address);
+        if(list==null)
+            map.put(address, list=new Entry(address));
+        list.add(channel);
     }
 }
