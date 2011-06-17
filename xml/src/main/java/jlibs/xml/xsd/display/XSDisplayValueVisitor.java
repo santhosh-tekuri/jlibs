75a8f34cf7592b5669c39fa5601fb889f1f066a3
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2011-06-17 01:42:38 +0000

    better value displayname for attributes

--- xml/src/main/java/jlibs/xml/xsd/display/XSDisplayValueVisitor.java
+++ xml/src/main/java/jlibs/xml/xsd/display/XSDisplayValueVisitor.java
@@ -49,62 +49,69 @@ public class XSDisplayValueVisitor extends PathReflectionVisitor<Object, String>
         }
         if(type instanceof XSComplexTypeDefinition)
             return null; //'{'+XSUtil.getQName(type, nsSupport)+'}';
-        else{
-            XSSimpleTypeDefinition simpleType = (XSSimpleTypeDefinition)type;
-            XSObjectList facets = simpleType.getMultiValueFacets();
-            StringBuilder buff = new StringBuilder();
-            for(int i=0; i<facets.getLength(); i++){
-                XSMultiValueFacet facet = (XSMultiValueFacet)facets.item(i);
-                switch(facet.getFacetKind()){
-                    case XSSimpleTypeDefinition.FACET_ENUMERATION:
-                        StringList list = facet.getLexicalFacetValues();
-                        for(int j=0; j<list.getLength(); j++){
-                            if(j!=0)
-                                buff.append("|");
-                            buff.append(list.item(j));
-                        }
-                }
-            }
-            if(buff.length()>0)
-                return buff.toString();
-
-            String min = null;
-            String max = null;
-            facets = simpleType.getFacets();
-            for(int i=0; i<facets.getLength(); i++){
-                XSFacet facet = (XSFacet)facets.item(i);
-                switch(facet.getFacetKind()){
-                    case XSSimpleTypeDefinition.FACET_MININCLUSIVE:
-                        min = '['+facet.getLexicalFacetValue();
-                        break;
-                    case XSSimpleTypeDefinition.FACET_MINEXCLUSIVE:
-                        min = '('+facet.getLexicalFacetValue();
-                        break;
-                    case XSSimpleTypeDefinition.FACET_MAXINCLUSIVE:
-                        max = facet.getLexicalFacetValue()+']';
-                        break;
-                    case XSSimpleTypeDefinition.FACET_MAXEXCLUSIVE:
-                        max = facet.getLexicalFacetValue()+')';
-                        break;
-                }
-            }
-            if(min!=null && max!=null)
-                return min+", "+max;
-            else if(min!=null || max!=null){
-                if(min!=null){
-                    String str = min.substring(1);
-                    return min.charAt(0)=='[' ? ">= "+str : "> "+str;
-                }else{
-                    String str = max.substring(0, max.length()-1);
-                    return max.charAt(max.length()-1)=='[' ? "<= "+str : "< "+str;
-                }
-            }
-
-            return XSUtil.getQName(type, nsSupport);
-        }
+        else
+            return process((XSSimpleTypeDefinition)type);
     }
 
     protected String process(XSAttributeUse attrUse){
-        return XSUtil.getQName(attrUse.getAttrDeclaration().getTypeDefinition(), nsSupport);
+        String value = process(attrUse.getAttrDeclaration().getTypeDefinition());
+        String constraintValue = attrUse.getConstraintValue();
+        if(constraintValue==null)
+            return value;
+        else
+            return value+' '+(attrUse.getConstraintType()==XSConstants.VC_DEFAULT ? "default(" : "fixed(")+constraintValue+')';
+    }
+
+    protected String process(XSSimpleTypeDefinition simpleType){
+        XSObjectList facets = simpleType.getMultiValueFacets();
+        StringBuilder buff = new StringBuilder();
+        for(int i=0; i<facets.getLength(); i++){
+            XSMultiValueFacet facet = (XSMultiValueFacet)facets.item(i);
+            switch(facet.getFacetKind()){
+                case XSSimpleTypeDefinition.FACET_ENUMERATION:
+                    StringList list = facet.getLexicalFacetValues();
+                    for(int j=0; j<list.getLength(); j++){
+                        if(j!=0)
+                            buff.append("|");
+                        buff.append(list.item(j));
+                    }
+            }
+        }
+        if(buff.length()>0)
+            return buff.toString();
+
+        String min = null;
+        String max = null;
+        facets = simpleType.getFacets();
+        for(int i=0; i<facets.getLength(); i++){
+            XSFacet facet = (XSFacet)facets.item(i);
+            switch(facet.getFacetKind()){
+                case XSSimpleTypeDefinition.FACET_MININCLUSIVE:
+                    min = '['+facet.getLexicalFacetValue();
+                    break;
+                case XSSimpleTypeDefinition.FACET_MINEXCLUSIVE:
+                    min = '('+facet.getLexicalFacetValue();
+                    break;
+                case XSSimpleTypeDefinition.FACET_MAXINCLUSIVE:
+                    max = facet.getLexicalFacetValue()+']';
+                    break;
+                case XSSimpleTypeDefinition.FACET_MAXEXCLUSIVE:
+                    max = facet.getLexicalFacetValue()+')';
+                    break;
+            }
+        }
+        if(min!=null && max!=null)
+            return min+", "+max;
+        else if(min!=null || max!=null){
+            if(min!=null){
+                String str = min.substring(1);
+                return min.charAt(0)=='[' ? ">= "+str : "> "+str;
+            }else{
+                String str = max.substring(0, max.length()-1);
+                return max.charAt(max.length()-1)=='[' ? "<= "+str : "< "+str;
+            }
+        }
+
+        return XSUtil.getQName(simpleType, nsSupport);
     }
 }
