a9662b3004e378aeb5919aa8ff316b7ec63db5c7
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2011-08-03 04:57:22 +0000

    NamespaceContext implementation for dom node

---
+++ trunk/xml/src/main/java/jlibs/xml/dom/DOMNamespaceContext.java
@@ -0,0 +1,119 @@
+package jlibs.xml.dom;
+
+import org.w3c.dom.Attr;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+
+import javax.xml.XMLConstants;
+import javax.xml.namespace.NamespaceContext;
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class DOMNamespaceContext implements NamespaceContext{
+    private Node node;
+
+    public DOMNamespaceContext(Node node){
+        this.node = node;
+    }
+
+    @Override
+    public String getNamespaceURI(String prefix){
+        if(prefix==null)
+            throw new IllegalArgumentException("prefix is null");
+        else if(XMLConstants.XML_NS_PREFIX.equals(prefix))
+            return XMLConstants.XML_NS_URI;
+        else if(XMLConstants.XMLNS_ATTRIBUTE.equals(prefix))
+            return XMLConstants.XMLNS_ATTRIBUTE_NS_URI;
+        else{
+            Attr attr = DOMUtil.findNamespaceDeclarationForPrefix(node, prefix);
+            if(attr!=null)
+                return attr.getNodeValue();
+            else if(XMLConstants.DEFAULT_NS_PREFIX.equals(prefix))
+                return XMLConstants.NULL_NS_URI;
+            else
+                return null;
+        }
+    }
+
+    @Override
+    public String getPrefix(String namespaceURI){
+        if(namespaceURI==null)
+            throw new IllegalArgumentException("namespaceURI is null");
+        else if(XMLConstants.XML_NS_URI.equals(namespaceURI))
+            return XMLConstants.XML_NS_PREFIX;
+        else if(XMLConstants.XMLNS_ATTRIBUTE_NS_URI.equals(namespaceURI))
+            return XMLConstants.XMLNS_ATTRIBUTE;
+        else{
+            Attr attr = DOMUtil.findNamespaceDeclarationForURI(node, namespaceURI);
+            if(attr!=null)
+                return attr.getLocalName();
+            else if(XMLConstants.NULL_NS_URI.equals(namespaceURI))
+                return XMLConstants.DEFAULT_NS_PREFIX;
+            else
+                return null;
+        }
+    }
+
+    @Override
+    public Iterator getPrefixes(String namespaceURI){
+        return new Iterator(node);
+    }
+
+    public class Iterator implements java.util.Iterator<String>{
+        private Node node;
+        private Set<String> prefixes = new HashSet<String>();
+        private int attrIndex = -1;
+
+        public Iterator(Node node){
+            this.node = node;
+            findNext();
+        }
+
+        private void findNext(){
+            do{
+                NamedNodeMap attrs = node.getAttributes();
+                if(attrs!=null){
+                    int length = attrs.getLength();
+                    for(++attrIndex; attrIndex<length; attrIndex++){
+                        Attr attr = (Attr)attrs.item(attrIndex);
+                        if(DOMUtil.isNamespaceDeclaration(attr) && !prefixes.contains(attr.getLocalName()))
+                            break;
+                    }
+                }
+                node = node.getParentNode();
+                attrIndex = -1;
+            }while(node!=null);
+        }
+
+        @Override
+        public boolean hasNext(){
+            return node!=null;
+        }
+
+        private Attr attr;
+
+        @Override
+        public String next(){
+            if(hasNext()){
+                attr = (Attr)node.getAttributes().item(attrIndex);
+                prefixes.add(attr.getLocalName());
+                findNext();
+                return attr.getLocalName();
+            }else
+                throw new NoSuchElementException();
+        }
+
+        public String getNamespaceURI(){
+            return attr.getNamespaceURI();
+        }
+
+        @Override
+        public void remove(){
+            throw new UnsupportedOperationException();
+        }
+    }
+}
