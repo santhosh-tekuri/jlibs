063b892e07b91b51c81036539848c8700e70895d
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2011-08-23 13:07:05 +0000

    utility methods: - to find subtypes of given complextype - to guess rootelements

--- xml/src/main/java/jlibs/xml/xsd/XSUtil.java
+++ xml/src/main/java/jlibs/xml/xsd/XSUtil.java
@@ -15,13 +15,17 @@
 
 package jlibs.xml.xsd;
 
+import jlibs.core.graph.*;
+import jlibs.core.graph.navigators.FilteredTreeNavigator;
+import jlibs.core.graph.walkers.PreorderWalker;
+import jlibs.xml.Namespaces;
 import jlibs.xml.sax.helpers.MyNamespaceSupport;
-import org.apache.xerces.xs.StringList;
-import org.apache.xerces.xs.XSAttributeUse;
-import org.apache.xerces.xs.XSModel;
-import org.apache.xerces.xs.XSObject;
+import org.apache.xerces.xs.*;
 
 import javax.xml.namespace.QName;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Santhosh Kumar T
@@ -46,5 +50,69 @@ public class XSUtil{
         String ns = obj.getNamespace()==null ? "" : obj.getNamespace();
         String prefix = nsSupport.findPrefix(ns);
         return new QName(ns, obj.getName(), prefix);
+    }
+
+    public static List<XSComplexTypeDefinition> getSubTypes(XSModel xsModel, XSComplexTypeDefinition complexType){
+        List<XSComplexTypeDefinition> subTypes = new ArrayList<XSComplexTypeDefinition>();
+        XSNamedMap namedMap = xsModel.getComponents(XSConstants.TYPE_DEFINITION);
+        XSObject anyType = namedMap.itemByName(Namespaces.URI_XSD, "anyType");
+        for(int i=0; i<namedMap.getLength(); i++){
+            XSObject item = namedMap.item(i);
+            if(item instanceof XSComplexTypeDefinition){
+                XSComplexTypeDefinition complexItem = (XSComplexTypeDefinition)item;
+                if(!complexItem.getAbstract()){
+                    do{
+                        complexItem = (XSComplexTypeDefinition)complexItem.getBaseType();
+                    }while(complexItem!=anyType && complexItem!=complexType);
+                    if(complexItem==complexType)
+                        subTypes.add((XSComplexTypeDefinition)item);
+                }
+            }
+        }
+        return subTypes;
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public static List<XSElementDeclaration> guessRootElements(final XSModel model){
+        XSNamedMap components = model.getComponents(XSConstants.ELEMENT_DECLARATION);
+        if(components.getLength()==0)
+            return Collections.emptyList();
+
+        final List<XSElementDeclaration> elements = new ArrayList<XSElementDeclaration>(components.getLength());
+        for(int i=0; i<components.getLength(); i++){
+            XSElementDeclaration elem = (XSElementDeclaration)components.item(i);
+            if(!elem.getAbstract())
+                elements.add(elem);
+        }
+
+        Filter filter = new Filter(){
+            @Override
+            public boolean select(Object elem){
+                return elem instanceof XSModel || elem instanceof XSElementDeclaration;
+            }
+        };
+        Navigator navigator = new FilteredTreeNavigator(new XSNavigator(), filter);
+        WalkerUtil.walk(new PreorderWalker(model, navigator), new Processor<Object>(){
+            @Override
+            public boolean preProcess(Object elem, Path path){
+                if(elements.size()>1 && path.getLength()>2){
+                    XSElementDeclaration elemDecl = (XSElementDeclaration)elem;
+                    if(elemDecl.getAbstract()){
+                        XSObjectList substitutionGroup = model.getSubstitutionGroup(elemDecl);
+                        for(int i=0; i<substitutionGroup.getLength(); i++){
+                            elements.remove(substitutionGroup.item(i));
+                            if(elements.size()==1)
+                                break;
+                        }
+                    }else
+                        elements.remove(elem);
+                }
+                return true;
+            }
+
+            @Override
+            public void postProcess(Object elem, Path path){}
+        });
+        return elements;
     }
 }
