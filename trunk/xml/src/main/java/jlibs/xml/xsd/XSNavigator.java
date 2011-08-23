3a362d1ce2753d0a45046f709910b919f20e6aca
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2011-08-23 13:07:49 +0000

    FIXED: xs:anyAttribute was being ignored

--- trunk/xml/src/main/java/jlibs/xml/xsd/XSNavigator.java
+++ trunk/xml/src/main/java/jlibs/xml/xsd/XSNavigator.java
@@ -15,17 +15,18 @@
 
 package jlibs.xml.xsd;
 
-import jlibs.core.graph.Sequence;
 import jlibs.core.graph.Navigator;
-import jlibs.core.graph.sequences.EmptySequence;
-import jlibs.core.graph.sequences.DuplicateSequence;
+import jlibs.core.graph.Sequence;
 import jlibs.core.graph.sequences.ConcatSequence;
+import jlibs.core.graph.sequences.DuplicateSequence;
+import jlibs.core.graph.sequences.EmptySequence;
 import jlibs.core.graph.visitors.ReflectionVisitor;
-import jlibs.xml.xsd.sequences.XSNamespaceItemListSequence;
 import jlibs.xml.xsd.sequences.XSNamedMapSequence;
+import jlibs.xml.xsd.sequences.XSNamespaceItemListSequence;
 import jlibs.xml.xsd.sequences.XSObjectListSequence;
-import static org.apache.xerces.xs.XSConstants.ELEMENT_DECLARATION;
 import org.apache.xerces.xs.*;
+
+import static org.apache.xerces.xs.XSConstants.ELEMENT_DECLARATION;
 
 /**
  * @author Santhosh Kumar T
@@ -54,10 +55,12 @@ public class XSNavigator extends ReflectionVisitor<Object, Sequence> implements 
     }
 
     protected Sequence<XSObject> process(XSComplexTypeDefinition complexType){
-        XSObjectListSequence<XSObject> attributes = new XSObjectListSequence<XSObject>(complexType.getAttributeUses());
+        Sequence<XSObject> sequence = new XSObjectListSequence<XSObject>(complexType.getAttributeUses());
+        if(complexType.getAttributeWildcard()!=null)
+            sequence = new ConcatSequence<XSObject>(sequence, new DuplicateSequence<XSObject>(complexType.getAttributeWildcard()));
         if(complexType.getParticle()!=null) // simple content
-            return new ConcatSequence<XSObject>(attributes, new DuplicateSequence<XSParticle>(complexType.getParticle()));
-        return attributes;
+            sequence = new ConcatSequence<XSObject>(sequence, new DuplicateSequence<XSParticle>(complexType.getParticle()));
+        return sequence;
     }
 
     protected Sequence<XSTerm> process(XSParticle particle){
