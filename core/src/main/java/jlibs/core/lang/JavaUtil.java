5529a625ae1df79afecf0a1a9d29a2c735b567ef
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2011-09-04 07:07:31 +0000

    dont allow java keyword as rule name

---
+++ core/src/main/java/jlibs/core/lang/JavaUtil.java
@@ -0,0 +1,43 @@
+/**
+ * JLibs: Common Utilities for Java
+ * Copyright (C) 2009  Santhosh Kumar T <santhosh.tekuri@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
+
+package jlibs.core.lang;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Santhosh Kumar T
+ */
+public class JavaUtil{
+    public static final List<String> KEYWORDS = Arrays.asList(
+        "abstract", "assert",
+        "boolean", "break", "byte",
+        "case", "catch", "char", "class", "const", "continue",
+        "default", "do", "double",
+        "else", "enum", "extends",
+        "false", "final", "finally", "float", "for",
+        "goto",
+        "if", "implements", "import", "instanceof", "int", "interface",
+        "long",
+        "native", "new", "null",
+        "package", "private", "protected", "public",
+        "return",
+        "short", "static", "strictfp", "super", "switch", "synchronized",
+        "this", "throw", "throws", "transient", "true", "try",
+        "void", "volatile",
+        "while"
+    );
+}
