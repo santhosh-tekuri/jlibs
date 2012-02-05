fa36e2d25851e05bcf37cdd531a0bda8c11051ca
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2012-02-05 06:27:04 +0000

    import and server command

--- wadl/src/main/java/jlibs/wadl/Command.java
+++ wadl/src/main/java/jlibs/wadl/Command.java
@@ -1,7 +1,13 @@
 package jlibs.wadl;
 
+import jlibs.core.net.URLUtil;
+import jlibs.wadl.model.Application;
+import jlibs.wadl.model.Resource;
+import jlibs.wadl.model.Resources;
 import jlibs.wadl.runtime.Path;
 
+import javax.xml.bind.JAXBContext;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -15,11 +21,13 @@ public class Command{
         this.terminal = terminal;
     }
 
-    public void run(String command){
+    public boolean run(String command) throws Exception{
         List<String> args = getArguments(command);
 
         String arg1 = args.get(0);
-        if(arg1.equals("cd")){
+        if(arg1.equals("import"))
+            importWADL(args.get(1));
+        else if(arg1.equals("cd")){
             Path path = terminal.getCurrentPath();
             if(args.size()==1)
                 path = path.getRoot();
@@ -35,7 +43,15 @@ public class Command{
                     terminal.getVariables().put(var, value);
                 }
             }
+        }else if(arg1.equals("target")){
+            if(args.size()==1)
+                terminal.setTarget(null);
+            else
+                terminal.setTarget(args.get(1));
+        }else if(arg1.equals("server")){
+            server(args.get(1));
         }
+        return true;
     }
     
     private List<String> getArguments(String command){
@@ -44,5 +60,48 @@ public class Command{
         while(stok.hasMoreTokens())
             args.add(stok.nextToken());
         return args;
+    }
+    
+    private void importWADL(String systemID) throws Exception{
+        JAXBContext jc = JAXBContext.newInstance(Application.class.getPackage().getName());
+        Application application = (Application)jc.createUnmarshaller().unmarshal(URLUtil.toURL(systemID));
+        Path root = null;
+        for(Resources resources: application.getResources()){
+            URI base = URI.create(resources.getBase());
+            String url = base.getScheme()+"://"+base.getHost();
+            if(base.getPort()!=-1)
+                url += ":"+base.getPort();
+            root = null;
+            for(Path path: terminal.getRoots()){
+                if(path.name.equals(url)){
+                    root = path;
+                    break;
+                }
+            }
+            if(root==null){
+                root = new Path(null, url, false);
+                terminal.getRoots().add(root);
+                if(base.getPath()!=null && !base.getPath().isEmpty())
+                    root = root.add(base.getPath());
+            }
+            for(Resource resource: resources.getResource()){
+                Path child = root.add(resource.getPath());
+                if(child.resource==null)
+                    child.resource = resource;
+                else
+                    child.resource.getMethodOrResource().addAll(resource.getMethodOrResource());
+            }
+        }
+        terminal.setCurrentPath(root);
+    }
+    
+    private void server(String server){
+        for(Path root: terminal.getRoots()){
+            if(root.name.equalsIgnoreCase(server)){
+                terminal.setCurrentPath(root);
+                terminal.setTarget(null);
+                return;
+            }
+        }
     }
 }
