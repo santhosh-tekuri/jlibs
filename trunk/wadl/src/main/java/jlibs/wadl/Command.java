62806f9977dec5d4c6e78823fe98a6f15bd83e03
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2012-02-05 09:48:16 +0000

    editor support

--- trunk/wadl/src/main/java/jlibs/wadl/Command.java
+++ trunk/wadl/src/main/java/jlibs/wadl/Command.java
@@ -1,14 +1,23 @@
 package jlibs.wadl;
 
+import jlibs.core.io.IOPump;
 import jlibs.core.net.URLUtil;
-import jlibs.wadl.model.Application;
-import jlibs.wadl.model.Resource;
-import jlibs.wadl.model.Resources;
+import jlibs.core.util.RandomUtil;
+import jlibs.wadl.model.*;
 import jlibs.wadl.runtime.Path;
+import jlibs.xml.sax.XMLDocument;
+import jlibs.xml.xsd.XSInstance;
+import jlibs.xml.xsd.XSParser;
+import org.apache.xerces.xs.XSModel;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -17,6 +26,8 @@ import java.util.StringTokenizer;
  */
 public class Command{
     private WADLTerminal terminal;
+    private Editor editor = new Editor();
+
     public Command(WADLTerminal terminal){
         this.terminal = terminal;
     }
@@ -50,7 +61,8 @@ public class Command{
                 terminal.setTarget(args.get(1));
         }else if(arg1.equals("server")){
             server(args.get(1));
-        }
+        }else
+            send(args);
         return true;
     }
     
@@ -65,6 +77,18 @@ public class Command{
     private void importWADL(String systemID) throws Exception{
         JAXBContext jc = JAXBContext.newInstance(Application.class.getPackage().getName());
         Application application = (Application)jc.createUnmarshaller().unmarshal(URLUtil.toURL(systemID));
+
+        XSModel schema = null;
+        if(application.getGrammars()!=null){
+            List<String> includes = new ArrayList<String>();
+            for(Include include: application.getGrammars().getInclude()){
+                if(include.getHref()!=null)
+                    includes.add(URLUtil.resolve(systemID, include.getHref()).toString());
+            }
+            if(!includes.isEmpty())
+                schema = new XSParser().parse(includes.toArray(new String[includes.size()]));
+        }
+        
         Path root = null;
         for(Resources resources: application.getResources()){
             URI base = URI.create(resources.getBase());
@@ -84,6 +108,7 @@ public class Command{
                 if(base.getPath()!=null && !base.getPath().isEmpty())
                     root = root.add(base.getPath());
             }
+            root.schema = schema;
             for(Resource resource: resources.getResource()){
                 Path child = root.add(resource.getPath());
                 if(child.resource==null)
@@ -103,5 +128,130 @@ public class Command{
                 return;
             }
         }
+    }
+    
+    private void send(List<String> args) throws Exception{
+        Path path = terminal.getCurrentPath();
+        if(path.resource==null){
+            System.err.println("resource not found");
+            return;
+        }
+
+        Method method = null;
+        for(Object obj: path.resource.getMethodOrResource()){
+            if(obj instanceof Method){
+                Method m = (Method)obj;
+                if(m.getName().equalsIgnoreCase(args.get(0))){
+                    method = m;
+                    break;
+                }
+            }
+        }
+        if(method==null){
+            System.err.println("unsupported method: "+args.get(0));
+            return;
+        }
+
+        StringBuilder buff = new StringBuilder();
+        Deque<Path> stack = terminal.getCurrentPath().getStack();
+        boolean first = true;
+        while(!stack.isEmpty()){
+            if(first){
+                first = false;
+                if(terminal.getTarget()!=null){
+                    stack.pop();
+                    buff.append(terminal.getTarget());
+                    continue;
+                }
+            }else
+                buff.append('/');
+            path = stack.pop();
+            if(path.variable()==null)
+                buff.append(path.name);
+            else{
+                String value = terminal.getVariables().get(path.variable());
+                if(value==null){
+                    System.err.println("unresolved variable: "+path.variable());
+                    return;
+                }
+                buff.append(value);
+            }
+        }
+        URL url = new URL(buff.toString());
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod(method.getName());
+
+        String payload = null;
+        Request request = method.getRequest();
+        if(request!=null){
+            if(!request.getRepresentation().isEmpty()){
+                Representation rep = request.getRepresentation().get(RandomUtil.random(0, request.getRepresentation().size()-1));
+                if(rep.getMediaType()!=null)
+                    con.addRequestProperty("Content-Type", rep.getMediaType());
+                if(rep.getElement()!=null){
+                    XSInstance xsInstance = new XSInstance();
+                    StringWriter writer = new StringWriter();
+                    XMLDocument xml = new XMLDocument(new StreamResult(writer), true, 4, null);
+                    xsInstance.generate(terminal.getCurrentPath().getSchema(), rep.getElement(), xml);
+                    payload = writer.toString();
+                }
+            }
+        }
+
+        if(payload!=null)
+            editor.show(payload, "text/xml");
+
+        String command = "tty vi "+payload;
+        final Process process = Runtime.getRuntime().exec(command);
+        new Thread(new IOPump(process.getInputStream(), System.out, false, false).asRunnable()).start();
+        OutputStream dummy = new OutputStream(){
+            @Override
+            public void write(int b) throws IOException{}
+
+            @Override
+            public void write(byte[] b) throws IOException{}
+
+            @Override
+            public void write(byte[] b, int off, int len) throws IOException{}
+        };
+        new Thread(new IOPump(process.getErrorStream(), dummy, false, false).asRunnable()).start();
+        OutputStream out = new OutputStream(){
+            private OutputStream delegate = process.getOutputStream();
+            @Override
+            public void write(int b) throws IOException{
+                delegate.write(b);
+                delegate.flush();
+            }
+
+            @Override
+            public void write(byte[] b) throws IOException{
+                delegate.write(b);
+                delegate.flush();
+            }
+
+            @Override
+            public void write(byte[] b, int off, int len) throws IOException{
+                delegate.write(b, off, len);
+                delegate.flush();
+            }
+        };
+        new Thread(new IOPump(System.in, out, false, false).asRunnable()).start();
+        process.waitFor();
+
+        if(payload!=null)
+            con.setDoOutput(true);
+        con.connect();
+        con.getOutputStream().write(payload.getBytes());
+
+        System.out.println(con.getResponseCode()+" "+con.getResponseMessage());
+        System.out.println();
+
+        InputStream in = con.getErrorStream();
+        if(in==null)
+            in = con.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        String line;
+        while((line=reader.readLine())!=null)
+            System.out.println(line);
     }
 }
