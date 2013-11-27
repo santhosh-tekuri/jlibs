2b7b4e6cc6c448fb284e1006338d8fe7eb3e6c9d
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2013-11-27 18:27:44 +0000

    allow user to supply sample values

---
+++ trunk/xml/src/main/java/jlibs/xml/xsd/XMLSampleValueGenerator.java
@@ -0,0 +1,87 @@
+package jlibs.xml.xsd;
+
+import jlibs.xml.sax.SAXUtil;
+import org.apache.xerces.xs.*;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.namespace.QName;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Generates sample values from given XML
+ *
+ * @author Santhosh Kumar T
+ */
+public class XMLSampleValueGenerator implements XSInstance.SampleValueGenerator{
+    private Map<XSElementDeclaration, String> elementValues = new HashMap<XSElementDeclaration, String>();
+    private Map<XSAttributeDeclaration, String> attributeValues = new HashMap<XSAttributeDeclaration, String>();
+
+    public XMLSampleValueGenerator(final XSModel schema, InputSource sampleInput) throws SAXException, ParserConfigurationException, IOException{
+        DefaultHandler handler = new DefaultHandler(){
+            private List<QName> xpath = new ArrayList<QName>();
+            private CharArrayWriter contents = new CharArrayWriter();
+
+            @Override
+            public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException{
+                xpath.add(new QName(uri, localName));
+                for(int i=0; i<attributes.getLength(); i++){
+                    QName qname = new QName(attributes.getURI(i), attributes.getLocalName(i));
+                    xpath.add(qname);
+                    XSAttributeDeclaration attr = XSUtil.findAttributeDeclaration(schema, xpath);
+                    if(attr!=null)
+                        attributeValues.put(attr, attributes.getValue(i));
+                    xpath.remove(xpath.size()-1);
+                }
+                contents.reset();
+            }
+
+
+            @Override
+            public void characters(char[] ch, int start, int length) throws SAXException{
+                contents.write(ch, start, length);
+            }
+
+            @Override
+            public void endElement(String uri, String localName, String qName) throws SAXException{
+                if(contents.size()>0){
+                    XSElementDeclaration elem = XSUtil.findElementDeclaration(schema, xpath);
+                    if(elem!=null){
+                        boolean simpleType = false;
+                        if(elem.getTypeDefinition().getTypeCategory()==XSTypeDefinition.SIMPLE_TYPE)
+                            simpleType = true;
+                        else{
+                            XSComplexTypeDefinition complexType = (XSComplexTypeDefinition)elem.getTypeDefinition();
+                            if(complexType.getContentType()==XSComplexTypeDefinition.CONTENTTYPE_SIMPLE)
+                                simpleType = true;
+                        }
+                        if(simpleType)
+                            elementValues.put(elem, contents.toString());
+                    }
+                }
+                xpath.remove(xpath.size()-1);
+                contents.reset();
+            }
+        };
+
+        SAXUtil.newSAXParser(true, false, false).parse(sampleInput, handler);
+    }
+
+    @Override
+    public String generateSampleValue(XSElementDeclaration element, XSSimpleTypeDefinition simpleType){
+        return elementValues.get(element);
+    }
+
+    @Override
+    public String generateSampleValue(XSAttributeDeclaration attribute, XSSimpleTypeDefinition simpleType){
+        return attributeValues.get(attribute);
+    }
+}
