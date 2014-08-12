afbd87540e2e0378952a923d3e0dc2d0b81171ff
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2014-08-12 14:35:54 +0000

    backup support in ReadFromInputStream

--- nio/src/main/java/jlibs/nio/async/ReadFromInputStream.java
+++ nio/src/main/java/jlibs/nio/async/ReadFromInputStream.java
@@ -15,8 +15,11 @@
 
 package jlibs.nio.async;
 
+import jlibs.nio.BufferPool;
+import jlibs.nio.Reactor;
 import jlibs.nio.channels.ListenerUtil;
 import jlibs.nio.channels.OutputChannel;
+import jlibs.nio.util.Bytes;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,12 +29,19 @@ import java.nio.ByteBuffer;
  * @author Santhosh Kumar Tekuri
  */
 public class ReadFromInputStream extends OutputTask{
+    private BufferPool pool = Reactor.current().bufferPool;
     private InputStream in;
     private ByteBuffer buffer;
+    private Bytes backup;
+
+    public ReadFromInputStream(InputStream in, Bytes backup){
+        this(in, backup.chunkSize);
+        this.backup = backup;
+    }
 
     public ReadFromInputStream(InputStream in, int chunkSize){
         this.in = in;
-        buffer = ByteBuffer.allocate(chunkSize);
+        buffer = pool.borrow(chunkSize);
         buffer.limit(0);
     }
 
@@ -40,8 +50,30 @@ public class ReadFromInputStream extends OutputTask{
         try{
             ready(out);
         }catch(Throwable thr){
-            ListenerUtil.resume(detach(out), thr, false);
+            resume(out, thr, false);
         }
+    }
+
+    private void resume(OutputChannel out, Throwable thr, boolean timeout){
+        if(buffer!=null){
+            if(backup==null || buffer.limit()==0)
+                pool.returnBack(buffer);
+            else{
+                buffer.position(0);
+                backup.append(buffer);
+            }
+            buffer = null;
+        }
+        try{
+            if(in!=null)
+                in.close();
+        }catch(IOException ex){
+            if(thr!=null)
+                thr.addSuppressed(ex);
+            else
+                Reactor.current().handleException(ex);
+        }
+        ListenerUtil.resume(detach(out), thr, timeout);
     }
 
     @Override
@@ -54,22 +86,51 @@ public class ReadFromInputStream extends OutputTask{
                 }
             }
             if(in==null){
-                ListenerUtil.resume(detach(out), null, false);
+                resume(out, null, false);
                 return;
             }
-            int read = in.read(buffer.array());
+            if(backup==null){
+                buffer.clear();
+                buffer.limit(0);
+            }else if(buffer.limit()==buffer.array().length){
+                buffer.clear();
+                backup.append(buffer);
+                buffer = pool.borrow(backup.chunkSize);
+                buffer.limit(0);
+            }
+            int read = in.read(buffer.array(), buffer.limit(), buffer.array().length-buffer.limit());
             if(read==-1){
-                in.close();
+                InputStream is = this.in;
                 in = null;
+                try{
+                    is.close();
+                }catch(IOException ex){
+                    Reactor.current().handleException(ex);
+                }
                 if(out.isFlushed())
-                    ListenerUtil.resume(detach(out), null, false);
+                    resume(out, null, false);
                 else
                     out.addWriteInterest();
                 return;
             }else{
-                buffer.position(0);
-                buffer.limit(read);
+                buffer.position(buffer.limit());
+                buffer.limit(buffer.limit()+read);
             }
         }
+    }
+
+    @Override
+    public void timeout(OutputChannel out) throws IOException{
+        resume(out, null, true);
+    }
+
+    @Override
+    public void error(OutputChannel out, Throwable thr){
+        resume(out, thr, false);
+    }
+
+    @Override
+    public void closed(OutputChannel out) throws IOException{
+        resume(out, null, false);
     }
 }
