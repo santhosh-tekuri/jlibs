edc6a38cb21086038d89bb88c6eceeb93896a2dd
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2014-08-19 21:42:40 +0000

    SET_DATE_HEADER option introduced

--- trunk/nio/src/main/java/jlibs/nio/http/msg/spec/HTTPDate.java
+++ trunk/nio/src/main/java/jlibs/nio/http/msg/spec/HTTPDate.java
@@ -17,8 +17,7 @@ package jlibs.nio.http.msg.spec;
 
 import jlibs.nio.http.msg.Version;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.text.*;
 import java.util.Date;
 import java.util.SimpleTimeZone;
 import java.util.TimeZone;
@@ -29,18 +28,7 @@ import java.util.TimeZone;
  * @author Santhosh Kumar Tekuri
  */
 public class HTTPDate extends SingleValueHeaderSpec<Date>{
-    private static final TimeZone GMT= new SimpleTimeZone(0, "GMT");
-    private static final ThreadLocal<SimpleDateFormat[]> FORMATS = ThreadLocal.withInitial(HTTPDate::formats);
-    static SimpleDateFormat[] formats(){
-        SimpleDateFormat formats[] = {
-            new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'"), // RFC 822, updated by RFC 1123
-            new SimpleDateFormat("EEEE, dd-MMM-yy HH:mm:ss 'GMT'"), // RFC 850, obsoleted by RFC 1036
-            new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy") // ANSI C's asctime() format
-        };
-        for(SimpleDateFormat sdf: formats)
-            sdf.setTimeZone(GMT);
-        return formats;
-    }
+    private static final ThreadLocal<Helper> HELPER = ThreadLocal.withInitial(Helper::new);
 
     public HTTPDate(String name){
         super(name);
@@ -48,22 +36,69 @@ public class HTTPDate extends SingleValueHeaderSpec<Date>{
 
     @Override
     protected Date _parse(String value, Version version){
-        if(value==null)
-            return null;
-
-        ParseException pe = null;
-        for(SimpleDateFormat sdf: FORMATS.get()){
-            try{
-                return sdf.parse(value);
-            }catch(ParseException ex){
-                pe = ex;
-            }
-        }
-        throw new RuntimeException(pe);
+        return value==null ? null : HELPER.get().parse(value);
     }
 
     @Override
     public String format(Date value, Version version){
-        return value==null ? null : FORMATS.get()[0].format(value);
+        return value==null ? null : HELPER.get().format(value);
+    }
+
+    public static String currentDate(){
+        return HELPER.get().currentDate();
+    }
+
+    private static class Helper{
+        private static final TimeZone GMT= new SimpleTimeZone(0, "GMT");
+        private static final FieldPosition DONT_CARE_FIELD_POSITION;
+        static{
+            FieldPosition fieldPositions[] = new FieldPosition[1];
+            new SimpleDateFormat(){
+                @Override
+                public StringBuffer format(Date date, StringBuffer toAppendTo, FieldPosition pos){
+                    fieldPositions[0] = pos;
+                    return super.format(date, toAppendTo, pos);
+                }
+            }.format(new Date());
+            DONT_CARE_FIELD_POSITION = fieldPositions[0];
+        }
+
+        private StringBuffer buffer = new StringBuffer(35);
+        private SimpleDateFormat formats[] = {
+                new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss 'GMT'"), // RFC 822, updated by RFC 1123
+                new SimpleDateFormat("EEEE, dd-MMM-yy HH:mm:ss 'GMT'"), // RFC 850, obsoleted by RFC 1036
+                new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy") // ANSI C's asctime() format
+        };
+
+        public Helper(){
+            for(SimpleDateFormat sdf: formats)
+                sdf.setTimeZone(GMT);
+        }
+
+        public Date parse(String value){
+            ParseException pe = null;
+            for(SimpleDateFormat sdf: formats){
+                try{
+                    return sdf.parse(value);
+                }catch(ParseException ex){
+                    pe = ex;
+                }
+            }
+            throw new RuntimeException(pe);
+        }
+
+        public String format(Date date){
+            buffer.setLength(0);
+            return formats[0].format(date, buffer, DONT_CARE_FIELD_POSITION).toString();
+        }
+
+        private String currentDate;
+        private long currentTime;
+        public String currentDate(){
+            long time = System.currentTimeMillis();
+            if(currentDate==null || time-currentTime>=1000)
+                currentDate = format(new Date(currentTime=time));
+            return currentDate;
+        }
     }
 }
