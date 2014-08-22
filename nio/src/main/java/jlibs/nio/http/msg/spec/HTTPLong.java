fa3f123808522f191ed57f5934a46382ef76711d
santhosh.tekuri@gmail.com <santhosh.tekuri@gmail.com@c7b8b07c-c7ab-11dd-86c2-0f8696b7b6f9>
2014-08-22 11:25:35 +0000

    faster string to long

--- nio/src/main/java/jlibs/nio/http/msg/spec/HTTPLong.java
+++ nio/src/main/java/jlibs/nio/http/msg/spec/HTTPLong.java
@@ -27,11 +27,40 @@ public class HTTPLong extends SingleValueHeaderSpec<Long>{
 
     @Override
     protected Long _parse(String value, Version version){
-        return value==null ? null : Long.parseLong(value);
+        return value==null ? null : parseLong(value);
     }
 
     @Override
     public String format(Long value, Version version){
         return value==null ? null : value.toString();
+    }
+
+    public static long parseLong(String value){
+        int len = value.length();
+        if(len==0)
+            throw new NumberFormatException("empty string");
+
+        int i = 0;
+        boolean negative = false;
+        char firstChar = value.charAt(0);
+        if(firstChar<'0'){
+            if(firstChar=='-')
+                negative = true;
+            else if(firstChar!='+')
+                throw new NumberFormatException("for input string \""+value+"\"");
+            if(len==1)
+                throw new NumberFormatException("cannot have lone + or -");
+            ++i;
+        }
+
+        int result = 0;
+        while(i<len){
+            char ch = value.charAt(i++);
+            if(ch>='0' && ch<='9')
+                result = result*10 + (ch-'0');
+            else
+                throw new NumberFormatException("for input string \""+value+"\"");
+        }
+        return negative ? -result : result;
     }
 }
