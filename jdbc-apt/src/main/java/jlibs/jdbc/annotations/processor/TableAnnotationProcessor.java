e001fb7286c84f456125d1be00fc9a7e3b128881
Santhosh Kumar Tekuri <santhosh.tekuri@gmail.com>
2015-07-26 19:23:51 +0530

    updated license header

--- jdbc-apt/src/main/java/jlibs/jdbc/annotations/processor/TableAnnotationProcessor.java
+++ jdbc-apt/src/main/java/jlibs/jdbc/annotations/processor/TableAnnotationProcessor.java
@@ -1,7 +1,7 @@
 /**
- * Copyright 2015 The JLibs Project
+ * Copyright 2015 Santhosh Kumar Tekuri
  *
- * The JLibs Project licenses this file to you under the Apache License,
+ * The JLibs authors license this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at:
  *
